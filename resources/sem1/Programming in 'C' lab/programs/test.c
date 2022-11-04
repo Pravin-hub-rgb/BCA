0015357a33f15e45fd62f64317046a0510057395
Pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2022-11-04 22:05:31 +0530

    operations and expression notes update 'C programming'

--- resources/sem1/Programming in 'C' lab/programs/test.c
+++ resources/sem1/Programming in 'C' lab/programs/test.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
 {
-    int a,b,sum;
-    printf("enter ther value of a and b");
-    scanf("%d%d",&a,&b);
-
-    sum=a+b;
-    printf("su,=%d",sum);
+    int x = 5, y, b = 10, c;
+    y = x++;
+    c = b--;
+    printf("The value of y is = %d and x is = %d\n", y, x); // The value of y is = 5 and x is = 6
+    printf("The value of c is = %d and c is = %d\n", c, b); // The value of c is = 10 and c is = 9
     return 0;
 }
