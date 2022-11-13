d70f567dc40f9e3a409cb03321225868a75eed05
Pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2022-11-13 18:01:47 +0530

    more updates on C notes

--- resources/sem1/Programming in 'C' lab/programs/test.c
+++ resources/sem1/Programming in 'C' lab/programs/test.c
@@ -5,9 +5,11 @@ int main()
    int n,fact=1;
    printf("Enter the number whose factorial you want to calculate : ");
    scanf("%d",&n);
-   for (int i = 1; i <= n; i++)
+   int i = 1;
+   while(i<=n)
    {
     fact*=i;
+    i++;
    }
    printf("The factorial of %d is %d\n",n,fact);
    return 0;
