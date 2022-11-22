19ec2d59b0774b1e2decb5762025038a680eb231
Pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2022-11-22 20:34:02 +0530

    C language notes update

--- resources/sem1/Programming in 'C' lab/programs/test.c
+++ resources/sem1/Programming in 'C' lab/programs/test.c
@@ -2,24 +2,13 @@
 
 int main()
 {
-   int a, b;
-   char op;
-   printf("Enter the value of 'a' and 'b' : ");
-   scanf("%d%d", &a, &b);
-   printf("Enter the operation from : '+', '-', '/', '*', '%%' : ");
-   scanf(" %c", &op);
-   if (op == '+')
-      printf("The sum of %d and %d is %d\n", a, b, a + b);
-   else if (op == '-')
-      printf("The subtraction of %d and %d is %d\n", a, b, a - b);
-   else if (op == '*')
-      printf("The multiplication of %d and %d is %d\n", a, b, a * b);
-   else if (op == '/')
-      printf("The division of %d and %d is %d\n", a, b, a / b);
-   else if (op == '%')
-      printf("The modulus of %d and %d is %d\n", a, b, a % b);
-   else
-      printf("You enter wrong operator\n");
-
+   for (int i = 1; i <= 5; i++)
+   {
+      for (int j = 1; j <= i; j++)
+      {
+         printf("%d ",i);
+      }
+      printf("\n");
+   }
    return 0;
 }
