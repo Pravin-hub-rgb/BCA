ba8d81cc65d4348fc4f0e6757db3e1f4e420b1b3
Pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2022-12-17 20:21:42 +0530

    notes update

---
+++ resources/sem1/Programming in 'C' lab/programs/tempCodeRunnerFile.c
@@ -0,0 +1,7 @@
+while (num > 0)
+//    {
+//       rev *= 10;
+//       rev += (num % 10);
+//       num /= 10;
+//    }
+//    printf("The reverse is : %d", rev);
--- resources/sem1/Programming in 'C' lab/programs/test.c
+++ resources/sem1/Programming in 'C' lab/programs/test.c
@@ -2,38 +2,14 @@
 
 int main()
 {
-   char op;
-   int a, b, c;
-   printf("Enter the value of a and b : ");
-   scanf("%d%d", &a, &b);
-   printf("Enter the operator : ");
-   scanf(" %c",&op);
-   switch (op)
-   {
-   case '+':
-      c = a + b;
-      printf("sum = %d\n", c);
-      break;
-   case '-':
-      c = a - b;
-      printf("subraction = %d\n", c);
-      break;
-   case '*':
-      c = a * b;
-      printf("multiplication = %d\n", c);
-      break;
-   case '/':
-      c = a / b;
-      printf("division = %d\n", c);
-      break;
-   case '%':
-      c = a % b;
-      printf("remainder = %d\n", c);
-      break;
-
-   default:
-      printf("Entered the wrong operator\n");
-      break;
-   }
+//    int num = 3435, rev = 0;
+   printf("Working");
+//    while (num > 0)
+//    {
+//       rev *= 10;
+//       rev += (num % 10);
+//       num /= 10;
+//    }
+//    printf("The reverse is : %d", rev);
    return 0;
 }
