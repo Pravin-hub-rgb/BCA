c6ffbbc80a5f4f0c2d49415e39f5cc9e0c932b47
Pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2022-12-20 16:46:16 +0530

    maths and C notes update

--- resources/sem1/Programming in 'C' lab/programs/test.c
+++ resources/sem1/Programming in 'C' lab/programs/test.c
@@ -2,14 +2,34 @@
 
 int main()
 {
-//    int num = 3435, rev = 0;
-   printf("Working");
-//    while (num > 0)
-//    {
-//       rev *= 10;
-//       rev += (num % 10);
-//       num /= 10;
-//    }
-//    printf("The reverse is : %d", rev);
+   int arr[50], n, search, flag = 0;
+   printf("Enter the number of element in array : ");
+   scanf("%d", &n);
+   printf("Enter the element in the array.\n");
+   for (int i = 0; i < n; i++)
+   {
+      printf("Enter element at %d index : ", i);
+      scanf("%d", &arr[i]);
+   }
+   printf("Enter the element you want to search : ");
+   scanf("%d", &search);
+
+   for (int i = 0; i < n; i++)
+   {
+      if (arr[i] == search)
+      {
+         flag = 1;
+         break;
+      }
+   }
+   if (flag)
+   {
+      printf("%d is present\n", search);
+   }
+   else
+   {
+      printf("%d is not present\n", search);
+   }
+
    return 0;
 }
