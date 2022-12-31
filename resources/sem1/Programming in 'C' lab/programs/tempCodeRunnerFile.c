ebbcc8206ad3f4ce7e0d6403554fb76b11945c19
Pravin-hub-rbg <pravvinkumarkumar@gmail.com>
2022-12-31 12:35:00 +0530

    C and maths notes update

---
+++ resources/sem1/Programming in 'C' lab/programs/tempCodeRunnerFile.c
@@ -0,0 +1,4 @@
+
+    //     printf("The entered string is palindrome");
+    // else
+    //     printf("The entered string is not palindrome");
--- resources/sem1/Programming in 'C' lab/programs/test.c
+++ resources/sem1/Programming in 'C' lab/programs/test.c
@@ -1,35 +1,19 @@
 #include <stdio.h>
-
+#include <math.h>
 int main()
 {
-   int arr[50], n, search, flag = 0;
-   printf("Enter the number of element in array : ");
-   scanf("%d", &n);
-   printf("Enter the element in the array.\n");
-   for (int i = 0; i < n; i++)
-   {
-      printf("Enter element at %d index : ", i);
-      scanf("%d", &arr[i]);
-   }
-   printf("Enter the element you want to search : ");
-   scanf("%d", &search);
-
-   for (int i = 0; i < n; i++)
-   {
-      if (arr[i] == search)
-      {
-         flag = 1;
-         break;
-      }
-   }
-   if (flag)
-   {
-      printf("%d is present\n", search);
-   }
-   else
-   {
-      printf("%d is not present\n", search);
-   }
-
-   return 0;
+    float a, b, c, r1, r2, d;
+    printf("Enter the values for equation : ");
+    scanf("%f%f%f", &a, &b, &c);
+    d = b * b - 4 * a * c;
+    if (d > 0)
+    {
+        r1 = (-b + sqrt(d)) / 2 * a;
+        r2 = (-b - sqrt(d)) / 2 * a;
+        printf("roots are real and unequal\n");
+        printf("%f\n%f\n", r1, r2);
+    }
+    else
+        printf("roots are imaginary");
+    return 0;
 }
