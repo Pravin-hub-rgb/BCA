19399f2712fb4fba466c24d79245f3b9e52e98d4
Pravin-hub-rbg <pravvinkumarkumar@gmail.com>
2023-01-08 12:58:10 +0530

    pc package lab notes update

--- resources/sem1/Programming in 'C' lab/programs/test.c
+++ resources/sem1/Programming in 'C' lab/programs/test.c
@@ -1,38 +1,16 @@
-#include <stdio.h>
-int main()
+#include<stdio.h>
+long fact(int);
+main()
 {
-    int arr1[50] = {0}, arr2[50] = {0}, sum[50], n1, n2, max_size;
-    printf("Enter the number of element in array one: ");
-    scanf("%d", &n1);
-    printf("Enter the element in the array.\n");
-    for (int i = 0; i < n1; i++)
-    {
-        printf("Enter element at %d index : ", i);
-        scanf("%d", &arr1[i]);
-    }
-    printf("Enter the number of element in array two: ");
-    scanf("%d", &n2);
-    printf("Enter the element in the array.\n");
-    for (int i = 0; i < n2; i++)
-    {
-        printf("Enter element at %d index : ", i);
-        scanf("%d", &arr2[i]);
-    }
-
-    if (n1 > n2)
-        max_size = n1;
-    else
-        max_size = n2;
-
-    for (int i = 0; i < max_size; i++)
-    {
-        sum[i] = arr1[1] + arr2[i];
-    }
-
-    // displaying array
-    for (int i = 0; i < max_size; i++)
-    {
-        printf("%d\t", sum[i]);
-    }
-    return 0;
+    int num;
+    printf("Enter a number : ");
+    scanf("%d", &num);
+    printf("Factorial of %d is %ld\n", num, fact(num));
+}
+long fact(int n)
+{
+    if (n == 0)
+        return 1;
+    else 
+        return (n * fact(n-1));
 }
