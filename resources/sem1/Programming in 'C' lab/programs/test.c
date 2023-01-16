c7ed1eb0fd707e9ac01f744ac66fe3a76d6ddfd7
Pravin-hub-rbg <pravvinkumarkumar@gmail.com>
2023-01-16 22:15:43 +0530

    maths and c programming notes added

--- resources/sem1/Programming in 'C' lab/programs/test.c
+++ resources/sem1/Programming in 'C' lab/programs/test.c
@@ -1,59 +1,11 @@
 #include <stdio.h>
-#define row 3
-#define col 3
+void printit(char words[])
+{
+   printf("The words are : %s", words);
+}
 int main()
 {
-   int ar[row][col], i, j, n;
-   printf("Enter the elements of array\n");
-   for (i = 0; i < row; i++)
-   {
-      for (j = 0; j < col; j++)
-      {
-         printf("Enter at ar[%d][%d] : ", i, j);
-         scanf("%d", &ar[i][j]);
-      }
-   }
-   printf("The elements of array\n");
-   for (i = 0; i < row; i++)
-   {
-      for (j = 0; j < col; j++)
-      {
-         printf(" %d ", ar[i][j]);
-      }
-      printf("\n");
-   }
-   if (row == col)
-   {
-      printf("The lower triangular matrix\n");
-      for (i = 0; i < row; i++)
-      {
-         for (j = 0; j < col; j++)
-         {
-            if (j <= i)
-            {
-               printf(" %d ", ar[i][j]);
-            }
-         }
-         printf("\n");
-      }
-      printf("The upper triangular matrix\n");
-      for (i = 0; i < row; i++)
-      {
-         for (j = 0; j < col; j++)
-         {
-            if (j >= i)
-            {
-               printf("  %d", ar[i][j]);
-            }
-            else
-            {
-               printf("   ");
-            }
-         }
-         printf("\n");
-      }
-   }
-   else
-      printf("Can't calculate the upper/lower triangle in matrix");
+   char words[20] = "hehe";
+   printit(words);
    return 0;
 }
