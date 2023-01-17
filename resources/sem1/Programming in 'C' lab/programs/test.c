cba004ff485dccfd05ce4ba253cc04298015f899
Pravin-hub-rbg <pravvinkumarkumar@gmail.com>
2023-01-17 21:13:38 +0530

    spelling error fixed

--- resources/sem1/Programming in 'C' lab/programs/test.c
+++ resources/sem1/Programming in 'C' lab/programs/test.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
-void printit(char words[])
-{
-   printf("The words are : %s", words);
-}
 int main()
 {
-   char words[20] = "hehe";
-   printit(words);
+   for(int i = 5;i>=1;i--)
+   {
+      for(int j=i;j>1;j--)
+      {
+         printf("_");
+      }
+      for(int k = 1;k<=5-i+1;k++)
+      {
+         printf("*");
+      }
+      printf("\n");
+   }
    return 0;
 }
