6bcf7d66ba4ce46fe2ddf68883e62e16f038e957
Pravin-hub-rbg <pravvinkumarkumar@gmail.com>
2023-03-22 20:43:31 +0530

    Class, pointer notes update

--- resources/sem2/OOPS_using_C++/test.cpp
+++ resources/sem2/OOPS_using_C++/test.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
 using namespace std;
-int area(int);
-int area(int, int);
-double area(double);
+class item
+{
+    int number;
+    float cost;
+
+public:
+    void getData(int, float);
+    void showData(void) // definition inside the function
+    {
+        cout << number << endl;
+        cout << cost << endl;
+    }
+};
+void item ::getData(int a, float b) // definition outside the function
+{
+    number = a;
+    cost = b;
+}
 int main()
 {
-    cout << "Area of square is : " << area(5) << endl;
-    cout << "Area of rectangle is : " << area(5, 10) << endl;
-    cout << "Area of circle is : " << area(3.8) << endl;
+    item x, y;
+    x.getData(100, 105.15);
+    x.showData();
+    y.getData(180, 195.15);
+    y.showData();
     return 0;
-}
-int area(int a)
-{
-    return a * a;
-}
-int area(int a, int b)
-{
-    return a * b;
-}
-double area(double a)
-{
-    return (314 * a * a) / 100;
 }
--- resources/sem2/advance_C/test.c
+++ resources/sem2/advance_C/test.c
@@ -1,77 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
-void push(int[], int*, int);
-void pop(int[], int*);
-void display(int[], int);
-// stack declaration
-
 int main()
 {
-   const int MAX = 5;
-   int stack[MAX], top = -1, choice;
-   while (1)
-   {
-      printf("\n\n**** MENU *****");
-      printf("\n1. PUSH");
-      printf("\n2. POP");
-      printf("\n3. DISPLAY");
-      printf("\n4. EXIT");
-      printf("\nEnter your choice : ");
-      scanf("%d", &choice);
-
-      switch (choice)
-      {
-      case 1:
-         push(stack, &top, MAX);
-         break;
-      case 2:
-         pop(stack, &top);
-         break;
-      case 3:
-         display(stack, top);
-         break;
-      case 4:
-         exit(0);
-      default:
-         printf("\nEnter a correct choice");
-      }
-   }
+   int var = 10, var2 = 20;
+   int *ptr;
+   int *q;
+   ptr = &var;
+   q = &var2;
+   *q = *ptr; // not this is valid
+   printf("%d", *q);
    return 0;
-}
-void push(int stack[], int *top, int MAX)
-{
-   if (*top >= MAX - 1)
-      printf("\n\nStack overflow\n\n");
-   else
-   {
-      (*top)++;
-      printf("\nEnter a value : ");
-      scanf("%d", &stack[*top]);
-      printf("\n\n%d is pushed\n\n", stack[*top]);
-   }
-}
-void pop(int stack[], int *top)
-{
-   int val;
-   if (*top <= -1)
-      printf("\n\nStack underflow\n\n");
-   else
-   {
-      val = stack[*top];
-      (*top)--;
-      printf("\n\n%d is popped\n\n", val);
-   }
-}
-void display(int stack[], int top)
-{
-   int i;
-   if (top <= -1)
-      printf("\n\nStack underflow\n\n");
-   else
-   {
-      for (i = top; i >= 0; i--)
-      {
-         printf("\n%d", stack[i]);
-      }
-   }
 }
