7745a22c973bbf3a123e47278b4e63d7fc3b1b82
Pravin-hub-rbg <pravvinkumarkumar@gmail.com>
2023-03-29 19:01:34 +0530

    notes update

--- resources/sem2/OOPS_using_C++/test.cpp
+++ resources/sem2/OOPS_using_C++/test.cpp
@@ -1,37 +1,94 @@
 #include <iostream>
 using namespace std;
-class employee
+
+void displayPer(int);
+
+class Marks
 {
-    int id;
-    int salary;
+    float percentage;
 
 public:
-    void getdata(void);
-    void putdata(void);
+    Marks() {}
+    Marks(int, int);
+    Marks(int, int, int);
+    Marks(int, int, int, int);
+    Marks(int, int, int, int, int);
 };
+
+Marks::Marks(int m1, int m2)
+{
+    percentage = (m1 + m2) / 2;
+    displayPer(percentage);
+}
+
+Marks::Marks(int m1, int m2, int m3)
+{
+    percentage = (m1 + m2 + m3) / 3;
+    displayPer(percentage);
+}
+
+Marks::Marks(int m1, int m2, int m3, int m4)
+{
+    percentage = (m1 + m2 + m3 + m4) / 4;
+    displayPer(percentage);
+}
+
+Marks::Marks(int m1, int m2, int m3, int m4, int m5)
+{
+    percentage = (m1 + m2 + m3 + m4 + m5) / 5;
+    displayPer(percentage);
+}
+
+void enterMarks(int);
+
+int m[5] = {0}, m1, m2, m3, m4, m5;
+
 int main()
 {
-    employee e[10]; // array of objects
-    for (int i = 0; i < 10; i++)
+    int sub;
+    Marks result;
+    cout << "----- Percentage Calculator------" << endl;
+    cout << "How many subjects do you have : ";
+    cin >> sub;
+    switch (sub)
     {
-        e[i].getdata();
-    }
-    for (int i = 0; i < 10; i++)
-    {
-        e[i].putdata();
+    case 2:
+        enterMarks(sub);
+        result = Marks(m1, m2);
+        break;
+    case 3:
+        enterMarks(sub);
+        result = Marks(m1, m2, m3);
+        break;
+    case 4:
+        enterMarks(sub);
+        result = Marks(m1, m2, m3, m4);
+        break;
+    case 5:
+        enterMarks(sub);
+        result = Marks(m1, m2, m3, m4, m5);
+        break;
+    default:
+        cout << "Enter subject between 1 and 6";
     }
     return 0;
 }
 
-void employee ::getdata()
+void enterMarks(int sub)
 {
-    cout << "Enter id : ";
-    cin >> id;
-    cout << "Enter salary : ";
-    cin >> salary;
+    for (int i = 1; i <= sub; i++)
+    {
+        cout << "Enter the marks in subject " << i << " : ";
+        cin >> m[i - 1];
+    }
+    m1 = m[0];
+    m2 = m[1];
+    m3 = m[2];
+    m4 = m[3];
+    m5 = m[4];
 }
-void employee ::putdata()
+
+void displayPer(int val)
 {
-    cout << "\nEnter id : " << id;
-    cout << "\nEnter salary : " << salary;
+    cout << " you got " << val << "%\n";
 }
