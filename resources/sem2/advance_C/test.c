fd33a4f6fdbac7711140d5f4e6f06dc0ca916cde
Pravin-hub-rbg <pravvinkumarkumar@gmail.com>
2023-04-06 23:53:38 +0530

    notes update

--- resources/sem2/advance_C/test.c
+++ resources/sem2/advance_C/test.c
@@ -1,36 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
+struct student
+{
+   int rollNo;
+   char section;
+   char name[20];
+};
+
 int main()
 {
-   int *ptr1, *ptr2;
-   int n, i;
-
-   printf("Enter the number of elements : ");
-   scanf("%d", &n);
-
-   // Dynamically allocate memory using malloc()
-   ptr1 = (int *)malloc(sizeof(int));
-
-   // Dynamically allocate memory using calloc()
-   ptr2 = (int *)calloc(n, sizeof(int));
-
-   // check if the memory has been successfully allocated by calloc or not
-   if (ptr1 == NULL || ptr2 == NULL)
-   {
-      printf("Memory not allocated.\n");
-      exit(0);
-   }
-   else
-   {
-      printf("Memory successfully allocated using malloc.\n");
-      // free ptr1 memory
-      free(ptr1);
-      printf("Malloc memory successfully freed.\n");
-
-      printf("Memory successfully allocated using calloc.\n");
-      // free ptr2 memory
-      free(ptr2);
-      printf("Calloc memory successfully freed.\n");
-   }
+   struct student s1 = {22, 'D', "Harry"};
+   struct student *ptr1, **ptr2;
+   ptr1 = &s1;
+   ptr2 = &ptr1;
+   (*ptr1).rollNo = 25;
+   printf("The roll no is %d", (*ptr1).rollNo);
+   printf("\nThe name is %s",(**ptr2).name);
+   printf("The roll no is %d", ptr1->rollNo);
+   printf("\nThe name is %s", (*ptr2)->name);
    return 0;
 }
