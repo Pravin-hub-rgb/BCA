5bfcfa47257220a20547670bde80081c8e9a7eba
Pravin-hub-rbg <pravvinkumarkumar@gmail.com>
2023-04-13 21:02:55 +0530

    notes update

--- resources/sem2/advance_C/tempCodeRunnerFile.c
+++ resources/sem2/advance_C/tempCodeRunnerFile.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-void swap(int *x, int *y)
+void swap(int x, int y)
 {
-    int temp = *x;
-    *x = *y;
-    *y = temp;
+    int temp = x;
+    x = y;
+    y = temp;
 }
 int main()
 {
     int a = 50;
     int b = 100;
     printf("\nBefore Swap : \n a = %d \n b = %d", a, b);
-    swap(&a, &b);
+    swap(a, b);
     printf("\n\nAfter Swap : \n a = %d \n b = %d\n", a, b);
     return 0;
 }
