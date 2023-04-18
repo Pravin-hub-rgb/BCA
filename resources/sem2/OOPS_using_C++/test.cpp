479299b02d3596f6e9cc0ed6d2c759855b3b786a
Pravin-hub-rbg <pravvinkumarkumar@gmail.com>
2023-04-18 11:47:48 +0530

    notes update

--- resources/sem2/OOPS_using_C++/test.cpp
+++ resources/sem2/OOPS_using_C++/test.cpp
@@ -1,31 +1,31 @@
 #include <iostream>
 using namespace std;
-class Sum
-{
-    int a, b;
-
-public:
-    Sum()
-    {
-        cout << "Constructor is called" << endl;
-        a = 1;
-        b = 2;
-    }
-    ~Sum()
-    {
-        cout << "Destructor is called using free()" << endl;
-    }
-    void show()
-    {
-        cout << "a = " << a << endl;
-        cout << "b = " << b << endl;
-    }
-};
+int area(int);
+float area(float);
+int area(int, int);
+double area(double);
 int main()
 {
-    Sum *ptr;
-    ptr = new Sum; // dynamic object creation
-    ptr->show(); // Accessing member through pointer to object
-    delete ptr; // Destroying object dynamically
+    cout << "Area of square is : " << area(5) << endl;
+    // cout << "Area of square  in floating point is : " << area(5.7) << endl; // this is not working properly
+    cout << "Area of square  in floating point is : " << area((float)5.7) << endl;
+    cout << "Area of rectangle is : " << area(5, 10) << endl;
+    cout << "Area of circle is : " << area(3.8) << endl;
     return 0;
+}
+int area(int a)
+{
+    return a * a;
+}
+float area(float a)
+{
+    return a * a;
+}
+int area(int a, int b)
+{
+    return a * b;
+}
+double area(double a)
+{
+    return (314 * a * a) / 100;
 }
