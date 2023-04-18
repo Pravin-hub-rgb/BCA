1d4df4f5a5ab01c0c125e9c39285c4401f60153f
Pravin-hub-rbg <pravvinkumarkumar@gmail.com>
2023-04-18 22:35:35 +0530

    C++ notes update

--- resources/sem2/OOPS_using_C++/test.cpp
+++ resources/sem2/OOPS_using_C++/test.cpp
@@ -1,35 +1,17 @@
-
 #include <iostream>
+#include <iomanip>
 using namespace std;
-class Distance
-{
-private:
-    int feet;
-    int inches;
-
-public:
-    friend void sum(Distance);
-    void show()
-    {
-        cout << "The distance is = " << feet << " Feet " << inches << " Inches";
-    }
-};
-
 int main()
 {
-    Distance one;
-    sum(one);
+    int num = 123;
+
+    cout << "Without using setw :" << num << "\n";
+    // Using setw() manipulator to set the width of the output field to 10
+    cout << "With using setw :" << setw(10) << num << "\n";
+
+    // Using endl manipulator to insert a newline
+    cout << "This is on one line." << endl;
+    cout << "This is on another line." << endl;
+
     return 0;
-}
-void sum(Distance d)
-{
-    int f1, f2, i1, i2;
-    cout << "Enter First Distance (in Feet and Inches) : ";
-    cin >> f1 >> i1;
-    cout << "Enter Second Distance (in Feet and Inches) : ";
-    cin >> f2 >> i2;
-    d.feet = f1 + f2;
-    d.inches = i1 + i2;
-    cout << "\nResult is : \n";
-    d.show();
 }
