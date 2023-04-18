62469a35c3b8be58b5108dc8abe42725c6796245
Pravin-hub-rbg <pravvinkumarkumar@gmail.com>
2023-04-18 20:44:18 +0530

    notes update

--- resources/sem2/OOPS_using_C++/test.cpp
+++ resources/sem2/OOPS_using_C++/test.cpp
@@ -1,17 +1,57 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
+class calculation
+{
+    int num1;
+    int num2;
+    int sum_v;
+    int sub_v;
+    int div_v;
+    int mut_v;
+
+public:
+    calculation();
+    void sum();
+    void sub();
+    void div();
+    void multiply();
+    void show();
+};
+calculation::calculation()
+{
+    num1 = 10;
+    num2 = 20;
+}
+void calculation::sum()
+{
+    sum_v = num1 + num2;
+}
+void calculation::sub()
+{
+    sub_v = num1 - num2;
+}
+void calculation::div()
+{
+    div_v = num1 / num2;
+}
+void calculation::multiply()
+{
+    mut_v = num1 * num2;
+}
+void calculation::show()
+{
+    cout << "The sum is = " << sum_v << endl;
+    cout << "The subtraction is = " << sub_v << endl;
+    cout << "The multiplication is = " << mut_v << endl;
+    cout << "The division is = " << div_v << endl;
+}
 int main()
 {
-    int num = 123;
-
-    cout << "Without using setw :" << num << "\n";
-    // Using setw() manipulator to set the width of the output field to 10
-    cout << "With using setw :" << setw(10) << num << "\n";
-
-    // Using endl manipulator to insert a newline
-    cout << "This is on one line." << endl;
-    cout << "This is on another line." << endl;
-
+    calculation x;
+    x.sum();
+    x.sub();
+    x.multiply();
+    x.div();
+    x.show();
     return 0;
 }
