da8f08fc09594bd6dee6396cba67c8f60f39d6e3
Pravin-hub-rbg <pravvinkumarkumar@gmail.com>
2023-05-05 08:57:33 +0530

    Bit manipulation notes udpate

--- resources/sem2/advance_C/test.c
+++ resources/sem2/advance_C/test.c
@@ -1,14 +1,11 @@
+
 #include <stdio.h>
 int main()
 {
-    int arr[5];
-    for (int i = 0; i < 5; i++)
-    {
-        scanf("%d", arr + i);
-    }
-    for (int i = 4; i >= 0; i--)
-    {
-        printf("%d\t", *(arr + i));
-    }
+    unsigned int a;
+    printf("Enter values of a : ");
+    scanf("%d", &a);
+    a = a >> 2;
+    printf("a >> 2 = %d\t\t", a);
     return 0;
 }
