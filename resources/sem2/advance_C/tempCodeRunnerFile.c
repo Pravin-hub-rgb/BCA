a31787f0315f3fd7d61f275abd5ec324b04b1ea7
Pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2023-05-20 12:26:05 +0530

    C notes update

--- resources/sem2/advance_C/tempCodeRunnerFile.c
+++ resources/sem2/advance_C/tempCodeRunnerFile.c
@@ -1,8 +1,2 @@
-se
-    // {
-    //     node *temp = foundNode->prev;
-    //     temp->next = foundNode->next;
-    //     foundNode->next->prev = temp;
-    //     free(foundNode);
-    //     foundNode = NULL;
-    // }
+ : ");
+    // fgets(str, 30, stdin);
