ff9b5bb85f2200702ab2099ac170dc0c03e7a64a
Pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2023-05-26 19:32:33 +0530

    Number of commits functionality added

--- public/main.js
+++ public/main.js
@@ -1,3 +1,43 @@
+document.addEventListener("DOMContentLoaded", function () {
+  updateCommitInfo();
+  // updateTimeAgo();
+  setInterval(updateCommitInfo, 3600000); // Update every hour
+});
+
+function updateCommitInfo() {
+  var username = "Pravin-hub-rgb";
+  var repo = "BCA";
+
+  // Make a GET request to the GitHub API to fetch the commit information
+  fetch(`https://api.github.com/repos/${username}/${repo}/stats/participation`)
+    .then(response => response.json())
+    .then(data => {
+      // Get the commit count and the timestamp of the latest commit
+      var commitCount = data.all.reduce((a, b) => a + b, 0);
+      // Update the HTML content
+      document.getElementById("commit-count1").textContent = commitCount;
+    })
+    .catch(error => {
+      console.log("Error:", error);
+    });
+  fetch(`https://api.github.com/repos/${username}/${repo}/commits`)
+    .then(response => response.json())
+    .then(data => {
+      var latestCommitTime = new Date(data[0].commit.author.date);
+      var currentTime = new Date();
+
+      // Calculate the time difference in hours
+      var timeDiffHours = Math.floor((currentTime - latestCommitTime) / (1000 * 60 * 60));
+
+      // Update the HTML content
+      document.getElementById("time-ago1").textContent = timeDiffHours === 1 ? "1 hour ago" : `${timeDiffHours} hours ago`;
+    })
+    .catch(error => {
+      console.log("Error:", error);
+    });
+}
+
+// ________________________________________________________________________________________
 function openNav() {
   document.getElementById("mySidepanel").style.width = "250px";
   allLi = document.querySelectorAll('.link')
@@ -46,7 +86,7 @@ hljs.highlightAll();
 
 // ************ Removing space in preTag *************
 const allPre = document.querySelectorAll('pre');
-allPre.forEach((tag)=>{
-    tag.firstChild.textContent = "";
-    tag.lastChild.textContent = "";
+allPre.forEach((tag) => {
+  tag.firstChild.textContent = "";
+  tag.lastChild.textContent = "";
 })
