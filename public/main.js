2fbbf1b9abf848e1d73efcc258ef1427ab962ec5
Pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2023-05-26 20:36:55 +0530

    Finally the commit count worked

--- public/main.js
+++ public/main.js
@@ -1,25 +1,39 @@
 document.addEventListener("DOMContentLoaded", function () {
   updateCommitInfo();
-  // updateTimeAgo();
   setInterval(updateCommitInfo, 3600000); // Update every hour
 });
 
 function updateCommitInfo() {
   var username = "Pravin-hub-rgb";
   var repo = "BCA";
+  var perPage = 100; // Number of commits to fetch per page
+  var currentPage = 1; // Current page number
+  var commitCount = 0; // Initialize the commit count
 
-  // Make a GET request to the GitHub API to fetch the commit information
-  fetch(`https://api.github.com/repos/${username}/${repo}/stats/participation`)
-    .then(response => response.json())
-    .then(data => {
-      // Get the commit count and the timestamp of the latest commit
-      var commitCount = data.all.reduce((a, b) => a + b, 0);
-      // Update the HTML content
-      document.getElementById("commit-count1").textContent = commitCount;
-    })
-    .catch(error => {
-      console.log("Error:", error);
-    });
+  // Fetch all commits using pagination
+  fetchCommits();
+
+  function fetchCommits() {
+    fetch(`https://api.github.com/repos/${username}/${repo}/commits?per_page=${perPage}&page=${currentPage}`)
+      .then(response => response.json())
+      .then(data => {
+        // Add the number of commits in the current page to the commit count
+        commitCount += data.length;
+
+        // If there are more pages, fetch the next page
+        if (data.length === perPage) {
+          currentPage++;
+          fetchCommits();
+        } else {
+          // All commits have been fetched, update the HTML content
+          document.getElementById("commit-count").textContent = commitCount;
+        }
+      })
+      .catch(error => {
+        console.log("Error:", error);
+      });
+  }
+
   fetch(`https://api.github.com/repos/${username}/${repo}/commits`)
     .then(response => response.json())
     .then(data => {
@@ -30,7 +44,7 @@ function updateCommitInfo() {
       var timeDiffHours = Math.floor((currentTime - latestCommitTime) / (1000 * 60 * 60));
 
       // Update the HTML content
-      document.getElementById("time-ago1").textContent = timeDiffHours === 1 ? "1 hour ago" : `${timeDiffHours} hours ago`;
+      document.getElementById("time-ago").textContent = timeDiffHours === 1 ? "1 hour ago" : `${timeDiffHours} hours ago`;
     })
     .catch(error => {
       console.log("Error:", error);
