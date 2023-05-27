452ab1fecebd81f7e508000eda91e3969b3c2afc
Pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2023-05-27 17:17:59 +0530

    notes update

--- resources/sem2/datastructure_and_FO/test.c
+++ resources/sem2/datastructure_and_FO/test.c
@@ -175,10 +175,57 @@ void quickSort(int A[], int l, int h)
         quickSort(A, j + 1, h);
     }
 }
+void merge(int A[], int l, int mid, int h)
+{
+    int i = l, j = mid + 1, k = l;
+    int B[h+1];
+    while(i <= mid && j <= h)
+    {
+        if(A[i] < A[j])
+            B[k++] = A[i++];
+        else 
+            B[k++] = A[j++];
+    }
+    for(; i <= mid; i++)
+        B[k++] = A[i];
+    for(; j <=h; j++)
+        B[k++] = A[j];
+    
+    for(i = l; i <= h; i++) A[i] = B[i];
+}
+void iMergeSort(int A[], int n)
+{
+    int p, l, h, mid, i;
+    for(p = 2; p <= n; p = p * 2)
+    {
+        for(i = 0; i + p - 1 < n; i = i + p)
+        {
+            l = i;
+            h = i + p - 1;
+            mid = (l + h) / 2;
+            merge(A, l, mid, h);
+        }
+    }
+    if(p/2 < n)
+    {
+        merge(A, 0, p/2 - 1, n);
+    }
+}
+void mergeSort(int A[], int l, int h)
+{
+    int mid;
+    if(l < h)
+    {
+        mid = ( l + h ) / 2;
+        mergeSort(A, l, mid);
+        mergeSort(A, mid + 1, h);
+        merge(A, l, mid, h);
+    }
+}
 int main()
 {
     int A[] = {11, 13, 7, 12, 16, 9, 24, 5, 10, 3}, n = 10, i;
-    quickSort(A, 0, n);
+    mergeSort(A, 0, n - 1);
     printf("\nDisplay the sorted array : ");
     for (i = 0; i < n; i++)
     {
