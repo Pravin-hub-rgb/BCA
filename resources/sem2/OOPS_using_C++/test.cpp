9edd9b1bca4b6c549efe524fb163158228166277
Pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2023-06-03 22:23:37 +0530

    notes update

--- resources/sem2/OOPS_using_C++/test.cpp
+++ resources/sem2/OOPS_using_C++/test.cpp
@@ -1,45 +1,99 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
-class A
-{
-public:
-    int x, y;
-    A()
-    {
-        x = 10;
-        y = 10;
+
+// Function to draw the tic-tac-toe board
+void drawBoard(const vector<vector<char>>& board) {
+    cout << "-------------" << endl;
+    for (int i = 0; i < 3; ++i) {
+        cout << "| ";
+        for (int j = 0; j < 3; ++j) {
+            cout << board[i][j] << " | ";
+        }
+        cout << endl << "-------------" << endl;
     }
-};
-class B : public A
-{
-public:
-    int sum;
-    B()
-    {
-        sum = x + y;
+}
+
+// Function to check if a player has won
+bool checkWin(const vector<vector<char>>& board, char player) {
+    // Check rows
+    for (int i = 0; i < 3; ++i) {
+        if (board[i][0] == player && board[i][1] == player && board[i][2] == player)
+            return true;
     }
-};
-class C
-{
-public:
-    int z;
-    C()
-    {
-        z = 20;
+
+    // Check columns
+    for (int j = 0; j < 3; ++j) {
+        if (board[0][j] == player && board[1][j] == player && board[2][j] == player)
+            return true;
     }
-};
-class D : public B, public C
-{
-public:
-    int total_sum;
-    D()
-    {
-        total_sum = sum + z;
+
+    // Check diagonals
+    if (board[0][0] == player && board[1][1] == player && board[2][2] == player)
+        return true;
+
+    if (board[0][2] == player && board[1][1] == player && board[2][0] == player)
+        return true;
+
+    return false;
+}
+
+// Function to play the tic-tac-toe game
+void playGame() {
+    vector<vector<char>> board(3, vector<char>(3, ' '));
+    char currentPlayer = 'X';
+    int row, col;
+
+    while (true) {
+        // Draw the board
+        drawBoard(board);
+
+        // Get the current player's move
+        cout << "Player " << currentPlayer << ", enter your move (row column): ";
+        cin >> row >> col;
+
+        // Check if the move is valid
+        if (row < 0 || row >= 3 || col < 0 || col >= 3 || board[row][col] != ' ') {
+            cout << "Invalid move. Try again." << endl;
+            continue;
+        }
+
+        // Make the move
+        board[row][col] = currentPlayer;
+
+        // Check if the current player has won
+        if (checkWin(board, currentPlayer)) {
+            drawBoard(board);
+            cout << "Player " << currentPlayer << " wins!" << endl;
+            break;
+        }
+
+        // Check if it's a draw
+        bool isDraw = true;
+        for (const auto& row : board) {
+            for (char cell : row) {
+                if (cell == ' ') {
+                    isDraw = false;
+                    break;
+                }
+            }
+            if (!isDraw)
+                break;
+        }
+
+        if (isDraw) {
+            drawBoard(board);
+            cout << "It's a draw!" << endl;
+            break;
+        }
+
+        // Switch players
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
     }
-};
-int main()
-{
-    D d_one;
-    cout << "The total sum is = " << d_one.total_sum << endl;
+}
+
+int main() {
+    playGame();
     return 0;
 }
