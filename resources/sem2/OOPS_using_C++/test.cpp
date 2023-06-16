a1618e4f6b10c65296d5f2b72749688ceb96c6aa
Pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2023-06-16 16:22:26 +0530

    notes upate

--- resources/sem2/OOPS_using_C++/test.cpp
+++ resources/sem2/OOPS_using_C++/test.cpp
@@ -1,25 +1,35 @@
 #include <iostream>
+#include <map>
 using namespace std;
 
 int main()
 {
-    int a, b;
-    float r;
-    cout << "Enter the value of a : ";
-    cin >> a;
-    try
+    map<string, int> myMap = {{"John", 25}, {"Alice", 30}, {"Bob", 40}};
+
+    // Access values using keys
+    cout << "Age of Alice: " << myMap["Alice"] << endl;
+
+    // Iterate over the map using a for loop
+    cout << "Name - Age pairs: ";
+    for (const auto &pair : myMap)
     {
-        cout << "Enter the value of b : ";
-        cin >> b;
-        if (b == 0)
-            throw 0;
-        r = a / b;
-        cout << "The result if : " << r << endl;
+        cout << pair.first << ":" << pair.second << " ";
     }
-    catch (int)
+    cout << endl;
+
+    // Check if a key exists in the map
+    string name = "John";
+    if (myMap.count(name) > 0)
     {
-        cout << "cannot divide by zero" << endl;
-        ;
+        cout << name << " exists in the map" << endl;
     }
+    else
+    {
+        cout << name << " does not exist in the map" << endl;
+    }
+
+    // Erase an element from the map
+    myMap.erase("Bob");
+
     return 0;
 }
