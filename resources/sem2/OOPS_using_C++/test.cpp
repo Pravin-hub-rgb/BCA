bfc4aaa1c2e87e627dd4c1abbe68513675b6dd98
Pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2023-06-16 20:26:58 +0530

    notes udpate

--- resources/sem2/OOPS_using_C++/test.cpp
+++ resources/sem2/OOPS_using_C++/test.cpp
@@ -1,35 +1,36 @@
 #include <iostream>
-#include <map>
 using namespace std;
+class Shape // abstract class
+{
+public:
+    virtual void draw() = 0; // Pure virtual function
+};
+
+class Circle : public Shape
+{
+public:
+    void draw() override
+    {
+        cout << "Drawing Circle." << endl;
+    }
+};
+
+class Rectangle : public Shape
+{
+public:
+    void draw() override
+    {
+        cout << "Drawing Rectangle." << endl;
+    }
+};
 
 int main()
 {
-    map<string, int> myMap = {{"John", 25}, {"Alice", 30}, {"Bob", 40}};
+    Circle circle;
+    Rectangle rectangle;
 
-    // Access values using keys
-    cout << "Age of Alice: " << myMap["Alice"] << endl;
-
-    // Iterate over the map using a for loop
-    cout << "Name - Age pairs: ";
-    for (const auto &pair : myMap)
-    {
-        cout << pair.first << ":" << pair.second << " ";
-    }
-    cout << endl;
-
-    // Check if a key exists in the map
-    string name = "John";
-    if (myMap.count(name) > 0)
-    {
-        cout << name << " exists in the map" << endl;
-    }
-    else
-    {
-        cout << name << " does not exist in the map" << endl;
-    }
-
-    // Erase an element from the map
-    myMap.erase("Bob");
+    circle.draw();
+    rectangle.draw();
 
     return 0;
 }
