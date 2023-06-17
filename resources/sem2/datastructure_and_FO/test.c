4bc12c586b5139d386dbae7914dab656b6369fd4
Pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2023-06-17 22:14:20 +0530

    notes update

--- resources/sem2/datastructure_and_FO/test.c
+++ resources/sem2/datastructure_and_FO/test.c
@@ -1,224 +1,48 @@
 #include <stdio.h>
-#include <stdlib.h>
-typedef struct node
+void swap(int *a, int *b)
 {
-    int data;
-    struct node *next;
-} node;
-node *start = NULL;
-
-void insertAtFront();
-void insertAtBack();
-void insertAfterNode();
-void deleteAtFront();
-void deleteAtBack();
-void deleteNode();
-void display();
-
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int partition(int A[], int l, int h)
+{
+    int pivot = A[l];
+    int i = l, j = h;
+    do
+    {
+        do
+        {
+            i++;
+        } while (A[i] <= pivot);
+        do
+        {
+            j--;
+        } while (A[j] > pivot);
+        if (i < j)
+            swap(&A[i], &A[j]);
+    } while (i < j);
+    swap(&A[l], &A[j]);
+    return j;
+}
+void quickSort(int A[], int l, int h)
+{
+    int j;
+    if (l < h)
+    {
+        j = partition(A, l, h);
+        quickSort(A, l, j);
+        quickSort(A, j + 1, h);
+    }
+}
 int main()
 {
-    int choice;
-    while (1)
+    int A[] = {11, 13, 7, 12, 16, 9, 24, 5, 10, 3}, n = 10, i;
+    quickSort(A, 0, n);
+    printf("\nDisplay the sorted array : ");
+    for (i = 0; i < n; i++)
     {
-        printf("\n------- MENU Circular single linked list ---------");
-        printf("\n1. Insert at front");
-        printf("\n2. Insert at back");
-        printf("\n3. Insert after a node");
-        printf("\n4. Delete at front");
-        printf("\n5. Delete at back");
-        printf("\n6. Delete a node");
-        printf("\n7. Display");
-        printf("\n8. Exit");
-        printf("\nEnter you choice : ");
-        scanf("%d", &choice);
-        switch (choice)
-        {
-        case 1:
-            insertAtFront();
-            break;
-        case 2:
-            insertAtBack();
-            break;
-        case 3:
-            insertAfterNode();
-            break;
-        case 4:
-            deleteAtFront();
-            break;
-        case 5:
-            deleteAtBack();
-            break;
-        case 6:
-            deleteNode();
-            break;
-        case 7:
-            display();
-            break;
-        case 8:
-            exit(0);
-        default:
-            printf("\nEnter a correct choice");
-        }
+        printf("%d ", A[i]);
     }
     return 0;
 }
-void insertAtFront()
-{
-    node *newNode = (node *)malloc(sizeof(node));
-    printf("Enter a value : ");
-    scanf("%d", &newNode->data);
-    if (start == NULL)
-    {
-        start = newNode;
-        newNode->next = start;
-    }
-    else
-    {
-        newNode->next = start;
-        start = newNode;
-    }
-}
-
-void insertAtBack()
-{
-    node *newNode = (node *)malloc(sizeof(node));
-    node *last = start;
-    printf("Enter a value : ");
-    scanf("%d", &newNode->data);
-    newNode->next = NULL;
-    if (start == NULL)
-    {
-        start = newNode;
-        newNode->next = start;
-    }
-    else
-    {
-        while (last->next != start)
-            last = last->next;
-        last->next = newNode;
-        newNode->next = start;
-    }
-}
-
-void insertAfterNode()
-{
-    if (start == NULL)
-    {
-        printf("\nlist is empty, cannot insert after a node\n");
-        return;
-    }
-    int node_val;
-    printf("\nEnter a node value after which you want to insert : ");
-    scanf("%d", &node_val);
-    node *temp = start;
-    while (temp->data != node_val)
-    {
-        if (temp->next == NULL)
-        {
-            printf("\nValue not found");
-            return;
-        }
-        temp = temp->next;
-    }
-    node *newNode = (node *)malloc(sizeof(node));
-    newNode->next = NULL;
-    printf("\nEnter the value : ");
-    scanf("%d", &newNode->data);
-    newNode->next = temp->next;
-    temp->next = newNode;
-}
-
-void deleteAtFront()
-{
-    if (start == NULL)
-    {
-        printf("\nNo node exist");
-        return;
-    }
-    if (start->next == start) // there is only one element
-    {
-        free(start);
-        start = NULL;
-        return;
-    }
-    node *last = start, *temp = start;
-    while (last->next != start)
-        last = last->next;
-    start = start->next;
-    last->next = start;
-    free(temp);
-}
-
-void deleteAtBack()
-{
-    if (start == NULL)
-    {
-        printf("\nNo node exist");
-        return;
-    }
-    if (start->next == start) // only one element left
-    {
-        free(start);
-        start = NULL;
-        return;
-    }
-    node *temp = start;
-    while (temp->next->next != start)
-    {
-        temp = temp->next;
-    }
-    free(temp->next);
-    temp->next = start;
-}
-
-void deleteNode()
-{
-    if (start == NULL)
-    {
-        printf("\nNo node exist");
-        return;
-    }
-    int search;
-    printf("Enter a value you want to delete : ");
-    scanf("%d", &search);
-    node *temp = start;
-    while (temp->data != search)
-    {
-        temp = temp->next;
-        if (temp->next == NULL)
-        {
-            printf("\nValue not found");
-            return;
-        }
-    }
-    if (temp == start)
-        deleteAtFront();
-    else if (temp->next == start)
-        deleteAtBack();
-    else
-    {
-        node *prev = start;
-        while (prev->next != temp)
-        {
-            prev = prev->next;
-        }
-        prev->next = temp->next;
-    }
-}
-
-void display()
-{
-    if (start == NULL)
-    {
-        printf("\nEmpty list");
-        return;
-    }
-    node *newNode = start;
-    while (newNode->next != start)
-    {
-        printf("%d --> ", newNode->data);
-        newNode = newNode->next;
-    }
-    printf("%d --> ", newNode->data);
-}
-                   
