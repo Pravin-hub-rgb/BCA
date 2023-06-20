46197206491d167b6a6eeacf60375b2bd0c2b1fc
Pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2023-06-21 00:06:37 +0530

    notes update

--- resources/sem2/advance_C/test.c
+++ resources/sem2/advance_C/test.c
@@ -1,51 +1,23 @@
 #include <stdio.h>
 
-void decimalToBinary(int num)
-{
-    int binary[32];
-    int i = 0;
-
-    // Convert decimal to binary using bit manipulation
-    while (num > 0)
-    {
-        binary[i] = num & 1;
-        num >>= 1;
-        i++;
+int main() {
+    FILE *sourceFile = fopen("source.txt", "r");
+    FILE *destinationFile = fopen("destination.txt", "w");
+    
+    if (sourceFile == NULL || destinationFile == NULL) {
+        printf("Failed to open files.\n");
+        return 1;
     }
-
-    printf("Binary representation: ");
-
-    // Print binary representation in reverse order
-    for (int j = i - 1; j >= 0; j--)
-    {
-        printf("%d", binary[j]);
+    
+    int ch;
+    while ((ch = fgetc(sourceFile)) != EOF) { // The variable 'ch' is updated automatically with each call to fgetc(file) inside the while loop. 
+        fputc(ch, destinationFile);
     }
-
-    printf("\n");
-}
-
-void checkEvenOdd(int num)
-{
-    // Check the least significant bit to determine even/odd
-    if ((num & 1) == 0)
-    {
-        printf("The number is even.\n");
-    }
-    else
-    {
-        printf("The number is odd.\n");
-    }
-}
-
-int main()
-{
-    int decimalNum;
-
-    printf("Enter a decimal number: ");
-    scanf("%d", &decimalNum);
-
-    decimalToBinary(decimalNum);
-    checkEvenOdd(decimalNum);
-
+    
+    fclose(sourceFile);
+    fclose(destinationFile);
+    
+    printf("Data copied successfully.\n");
+    
     return 0;
 }
