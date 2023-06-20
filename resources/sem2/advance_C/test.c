78f442f0d806847e0009fba600885a2a6c6505ad
Pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2023-06-20 22:20:21 +0530

    notes update

--- resources/sem2/advance_C/test.c
+++ resources/sem2/advance_C/test.c
@@ -1,18 +1,51 @@
 #include <stdio.h>
-int isBitSet(int num, int bitPos)
+
+void decimalToBinary(int num)
 {
-    int mask = 1 << bitPos;
-    return ((num & mask) > 0);
+    int binary[32];
+    int i = 0;
+
+    // Convert decimal to binary using bit manipulation
+    while (num > 0)
+    {
+        binary[i] = num & 1;
+        num >>= 1;
+        i++;
+    }
+
+    printf("Binary representation: ");
+
+    // Print binary representation in reverse order
+    for (int j = i - 1; j >= 0; j--)
+    {
+        printf("%d", binary[j]);
+    }
+
+    printf("\n");
 }
+
+void checkEvenOdd(int num)
+{
+    // Check the least significant bit to determine even/odd
+    if ((num & 1) == 0)
+    {
+        printf("The number is even.\n");
+    }
+    else
+    {
+        printf("The number is odd.\n");
+    }
+}
+
 int main()
 {
-    int num = 10; // Binary: 0000 1010
+    int decimalNum;
 
-    // Check if bit at position 3 is set
-    if (isBitSet(num, 3))
-        printf("Bit at position 3 is set.\n");
-    else
-        printf("Bit at position 3 is not set.\n");
+    printf("Enter a decimal number: ");
+    scanf("%d", &decimalNum);
+
+    decimalToBinary(decimalNum);
+    checkEvenOdd(decimalNum);
 
     return 0;
 }
