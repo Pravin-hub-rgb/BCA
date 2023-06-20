86d4633575713555c6ec0a2196532debf3839fd5
Pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2023-06-20 18:02:38 +0530

    notes update

--- resources/sem2/advance_C/test.c
+++ resources/sem2/advance_C/test.c
@@ -1,20 +1,11 @@
-
 #include <stdio.h>
-#pragma pack(1)
-// space opmitized representation of 'order'
-struct order
-{
-    // isAvailable can store either 0 or 1, so 1 bits are sufficient
-    unsigned int isAvailable : 1;
-
-    // isTshirt can store either 0 or 1, so 1 bits are sufficient
-    unsigned int isTshirt : 1;
-};
-
 int main()
 {
-    printf("Size of order is %lu byte\n", sizeof(struct order)); // 1
-    struct order hoodie= {1, 0};
-    printf("The order is %s and it is %s",(hoodie.isAvailable)?"available":"not available", (hoodie.isTshirt)?"T-shirt":"not T-shirt");
+    int a = 10, b = 7;
+    printf("The value of a and b before swap : a = %d & b = %d\n", a, b);
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+    printf("The value of a and b after swap : a = %d & b = %d\n", a, b);
     return 0;
 }
