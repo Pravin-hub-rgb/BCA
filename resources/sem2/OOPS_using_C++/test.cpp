7dde9bd123bbbcff9dd3508dcea7950419c94121
Pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2023-06-22 00:21:00 +0530

    C++ notes update - String added

--- resources/sem2/OOPS_using_C++/test.cpp
+++ resources/sem2/OOPS_using_C++/test.cpp
@@ -1,31 +1,11 @@
 #include <iostream>
+#include <string>
 using namespace std;
-template <class Type>
-class Test
-{
-private:
-    Type n;
-
-public:
-    void getValue()
-    {
-        cin >> n;
-    }
-    void printVal()
-    {
-        cout << "you entered : " << n << endl;
-    }
-};
 int main()
 {
-    Test<int> objINT;
-    cout << "Enter integer value : ";
-    objINT.getValue();
-    objINT.printVal();
-    Test<float> objFLOAT;
-    cout << "Enter real value : ";
-    objFLOAT.getValue();
-    objFLOAT.printVal();
-
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Hello, " << name << "!";
     return 0;
 }
