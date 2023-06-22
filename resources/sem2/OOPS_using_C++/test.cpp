5d2d262be16271d16ac6134701c58e16c5e7f110
Pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2023-06-22 14:25:36 +0530

    notes update and quotes removed

--- resources/sem2/OOPS_using_C++/test.cpp
+++ resources/sem2/OOPS_using_C++/test.cpp
@@ -1,11 +1,65 @@
 #include <iostream>
-#include <string>
 using namespace std;
+
+class Myclass
+{
+private:
+    int value;
+
+public:
+    Myclass() {}
+
+    Myclass(int val)
+    {
+        value = val;
+    }
+
+    int getValue()
+    {
+        return value;
+    }
+
+    // Friend function for overloading the * operator
+    friend Myclass operator*(Myclass, Myclass);
+
+    // Friend function for overloading the % operator
+    friend Myclass operator%(Myclass, Myclass);
+
+    // Friend function for overloading the / operator
+    friend Myclass operator/(Myclass, Myclass);
+};
+
+Myclass operator*(Myclass obj1, Myclass obj2)
+{
+    int product = obj1.value * obj2.value;
+    return Myclass(product);
+}
+
+Myclass operator%(Myclass obj1, Myclass obj2)
+{
+    int remainder = obj1.value % obj2.value;
+    return Myclass(remainder);
+}
+
+Myclass operator/(Myclass obj1, Myclass obj2)
+{
+    int quotient = obj1.value / obj2.value;
+    return Myclass(quotient);
+}
+
 int main()
 {
-    string name;
-    cout << "Enter your name: ";
-    cin >> name;
-    cout << "Hello, " << name << "!";
+    Myclass obj1(30);
+    Myclass obj2(10);
+
+    Myclass obj3 = obj1 * obj2;
+    cout << "Product: " << obj3.getValue() << endl;
+
+    Myclass obj4 = obj1 % obj2;
+    cout << "Remainder: " << obj4.getValue() << endl;
+
+    Myclass obj5 = obj1 / obj2;
+    cout << "Quotient: " << obj5.getValue() << endl;
+
     return 0;
 }
