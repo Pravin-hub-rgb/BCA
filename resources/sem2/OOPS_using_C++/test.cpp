801156f142abfee61732c546399139b06d366811
Pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2023-06-22 21:34:59 +0530

    notes update

--- resources/sem2/OOPS_using_C++/test.cpp
+++ resources/sem2/OOPS_using_C++/test.cpp
@@ -1,65 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
-
-class Myclass
-{
-private:
-    int value;
-
-public:
-    Myclass() {}
-
-    Myclass(int val)
-    {
-        value = val;
-    }
-
-    int getValue()
-    {
-        return value;
-    }
-
-    // Friend function for overloading the * operator
-    friend Myclass operator*(Myclass, Myclass);
-
-    // Friend function for overloading the % operator
-    friend Myclass operator%(Myclass, Myclass);
-
-    // Friend function for overloading the / operator
-    friend Myclass operator/(Myclass, Myclass);
-};
-
-Myclass operator*(Myclass obj1, Myclass obj2)
-{
-    int product = obj1.value * obj2.value;
-    return Myclass(product);
-}
-
-Myclass operator%(Myclass obj1, Myclass obj2)
-{
-    int remainder = obj1.value % obj2.value;
-    return Myclass(remainder);
-}
-
-Myclass operator/(Myclass obj1, Myclass obj2)
-{
-    int quotient = obj1.value / obj2.value;
-    return Myclass(quotient);
-}
 
 int main()
 {
-    Myclass obj1(30);
-    Myclass obj2(10);
+    ifstream inputFile("input.txt"); // Open the file for reading
 
-    Myclass obj3 = obj1 * obj2;
-    cout << "Product: " << obj3.getValue() << endl;
-
-    Myclass obj4 = obj1 % obj2;
-    cout << "Remainder: " << obj4.getValue() << endl;
-
-    Myclass obj5 = obj1 / obj2;
-    cout << "Quotient: " << obj5.getValue() << endl;
+    if (inputFile.is_open())
+    { // Check if the file is successfully opened
+        string line;
+        while (getline(inputFile, line))
+        {                         // Read each line from the file
+            cout << line << endl; // Display the line on the console
+        }
+        inputFile.close(); // Close the file
+    }
+    else
+    {
+        cout << "Failed to open the file." << endl;
+    }
 
     return 0;
 }
