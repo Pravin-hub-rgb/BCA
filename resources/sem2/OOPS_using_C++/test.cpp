92872405189f8ba9115715499583aea0812c9356
Pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2023-07-03 12:36:24 +0530

    notes update

--- resources/sem2/OOPS_using_C++/test.cpp
+++ resources/sem2/OOPS_using_C++/test.cpp
@@ -1,24 +1,17 @@
-#include <iostream>
-using namespace std;
 
+#include <iostream>
+#include <fstream>
+using namespace std;
 int main()
 {
-    int a, b;
-    float r;
-    cout << "Enter the value of a : ";
-    cin >> a;
-    try
-    {
-        cout << "Enter the value of b : ";
-        cin >> b;
-        if (b == 0)
-            throw 0;
-        r = a / b;
-        cout << "The result if : " << r << endl;
-    }
-    catch (int)
-    {
-        cout << "cannot divide by zero" << endl;
-    }
+    int a;
+    ifstream file1;
+    file1.open("abc.txt");
+    file1 >> a;
+    cout << "1 =  " << a << endl;
+    file1 >> a;
+    cout << "2 =  " << a << endl;
+    file1 >> a;
+    cout << "3 =  " << a << endl;
     return 0;
 }
