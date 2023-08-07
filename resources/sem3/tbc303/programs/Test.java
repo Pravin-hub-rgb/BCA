2bdf21980e4b64923a4cca6125712b4c76454299
Pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2023-08-07 21:31:13 +0530

    notes update

--- resources/sem3/tbc303/programs/Test.java
+++ resources/sem3/tbc303/programs/Test.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class initial {
+class Initial {
     public int a;
     public int b;
     public int mult;
@@ -23,10 +23,9 @@ class initial {
     }
 }
 
-class Test{
-    public static void main(String args[])
-    {
-        initial obj = new initial();
+class Multiplication {
+    public static void main(String args[]) {
+        Initial obj = new Initial();
         obj.getValue();
         obj.calValue();
         obj.display();
