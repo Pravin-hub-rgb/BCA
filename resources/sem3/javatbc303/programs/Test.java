7ebb9a77448291608de36f60a53d4f8767305a81
Pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2023-08-16 21:21:48 +0530

    notes update

--- resources/sem3/javatbc303/programs/Test.java
+++ resources/sem3/javatbc303/programs/Test.java
@@ -1,21 +1,28 @@
-import java.util.Scanner;
+// class Test {
+// public static void main(String[] args) {
+// int number[] = { 55, 40, 80, 65, 72 };
+// int n = number.length;
+// System.out.println("Given list : ");
+// for (int i = 0; i < n; i++) {
+// System.out.print(" " + number[i]);
+// }
+// System.out.println(" ");
 
-class Test {
-    static Scanner input = new Scanner(System.in);
-    static int num;
+// // swapping logic
+// for (int i = 0; i < n; i++) {
+// for (int j = 0; j < n - i - 1; j++) {
+// if (number[j] > number[j + 1]) {
+// int temp = number[j];
+// number[j] = number[j + 1];
+// number[j + 1] = temp;
+// }
+// }
+// }
 
-    public static void main(String args[]) {
-        System.out.println("Enter your number : ");
-        num = input.nextInt();
-        System.out.println("Sum of digits = " + sumOfDigits(num));
-    }
-
-    public static int sumOfDigits(int num) {
-        int sum = 0;
-        while (num > 0) {
-            sum = sum + (num % 10);
-            num /= 10;
-        }
-        return sum;
-    }
-}
+// System.out.println("Sorted list : ");
+// for (int i = 0; i < n; i++) {
+// System.out.print(" " + number[i]);
+// }
+// System.out.println(" ");
+// }
+// }
