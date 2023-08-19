5e822b281df5785aae63c9891d11aa4edd1d9f3e
Pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2023-08-19 22:11:02 +0530

    Discord plug and notes update

--- resources/sem3/javatbc303/programs/Test.java
+++ resources/sem3/javatbc303/programs/Test.java
@@ -1,28 +1,28 @@
-// class Test {
-// public static void main(String[] args) {
-// int number[] = { 55, 40, 80, 65, 72 };
-// int n = number.length;
-// System.out.println("Given list : ");
-// for (int i = 0; i < n; i++) {
-// System.out.print(" " + number[i]);
-// }
-// System.out.println(" ");
+class Test {
+    public static void main(String[] args) {
+        int arr1[] = { 2, 4, 6, 8, 99, 1 };
+        int arr2[] = { 2, 1, 2, 3, 9, 4 };
+        System.out.print("arr1 : ");
+        display(arr1);
+        System.out.print("arr2 : ");
+        display(arr2);
+        System.out.print("Sum of arr1 and arr2 : ");
+        sumTwoArray(arr1, arr2);
 
-// // swapping logic
-// for (int i = 0; i < n; i++) {
-// for (int j = 0; j < n - i - 1; j++) {
-// if (number[j] > number[j + 1]) {
-// int temp = number[j];
-// number[j] = number[j + 1];
-// number[j + 1] = temp;
-// }
-// }
-// }
+    }
 
-// System.out.println("Sorted list : ");
-// for (int i = 0; i < n; i++) {
-// System.out.print(" " + number[i]);
-// }
-// System.out.println(" ");
-// }
-// }
+    public static void sumTwoArray(int[] arr1, int[] arr2) {
+        int newArray[] = new int[arr1.length];
+        for (int i = 0; i < arr1.length; i++) {
+            newArray[i] = arr1[i] + arr2[i];
+        }
+        display(newArray);
+    }
+
+    public static void display(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("");
+    }
+}
