ab2f46dae795dcb68221361a4f42bb5a922dbef7
Pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2023-08-23 00:13:52 +0530

    notes update

--- resources/sem3/javatbc303/programs/Test.java
+++ resources/sem3/javatbc303/programs/Test.java
@@ -1,28 +1,21 @@
+import java.util.Scanner;
+
+// class Initial {
+//     void display() {
+//         System.out.println("Hellow world");
+//     }
+// }
+
+// WAP to retrieve number of digits in  a number
+
 class Test {
     public static void main(String[] args) {
-        int arr1[] = { 2, 4, 6, 8, 99, 1 };
-        int arr2[] = { 2, 1, 2, 3, 9, 4 };
-        System.out.print("arr1 : ");
-        display(arr1);
-        System.out.print("arr2 : ");
-        display(arr2);
-        System.out.print("Sum of arr1 and arr2 : ");
-        sumTwoArray(arr1, arr2);
-
-    }
-
-    public static void sumTwoArray(int[] arr1, int[] arr2) {
-        int newArray[] = new int[arr1.length];
-        for (int i = 0; i < arr1.length; i++) {
-            newArray[i] = arr1[i] + arr2[i];
+        int num = 3455;
+        int count = 0;
+        while (num > 0) {
+            count++;
+            num /= 10;
         }
-        display(newArray);
-    }
-
-    public static void display(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println("");
+        System.out.println("Number of digits = " + count);
     }
 }
