9642e5861f2b74e66f44c55dd56b10c59866c264
Pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2023-08-26 21:53:23 +0530

    notes update

--- resources/sem3/javatbc303/program/Test.java
+++ resources/sem3/javatbc303/program/Test.java
@@ -1,20 +1,21 @@
 class Test {
+    static String name[] = { "Madras", "Delhi", "Ahmedabad", "Calcutta", "Bombay" };
+
     public static void main(String[] args) {
-        // int[] numbers = new int[] { 10, 20, 30, 40, 50 }; // normal array
-
-        // Creating and using an anonymous array for integer values
-        // Passing the anonymous array to a method
-        int sum = calculateSum(new int[] { 5, 10, 15, 20 });
-
-        System.out.println("Sum of numbers: " + sum);
-    }
-
-    // Method to calculate the sum of an array
-    public static int calculateSum(int[] array) {
-        int sum = 0;
-        for (int i = 0; i < array.length; i++) {
-            sum += array[i];
+        int size = name.length;
+        String temp = null;
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                if (name[j].compareTo(name[i]) < 0) {
+                    // Swap the strings
+                    temp = name[i];
+                    name[i] = name[j];
+                    name[j] = temp;
+                }
+            }
         }
-        return sum;
+        for (int i = 0; i < size; i++) {
+            System.out.println(name[i]);
+        }
     }
 }
