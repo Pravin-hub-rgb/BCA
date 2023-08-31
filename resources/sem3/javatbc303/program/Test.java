7bf6c93e27e46e1d3d6a498ba62e0597b1a41fb1
Pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2023-08-31 23:01:52 +0530

    notes update

--- resources/sem3/javatbc303/program/Test.java
+++ resources/sem3/javatbc303/program/Test.java
@@ -1,29 +1,12 @@
-// wap in Java to count number of objects in java;
-// wap to state the difference between mutable and immmutable string
-// wap to multiply two matrices
-// class variable and instance variable
-class Keyboard {
-    static int count = 0;
-
-    Keyboard() {
-        count++;
-    }
-
-    void display() {
-        System.out.println("This is Keyboard class");
-    }
-}
-
 class Test {
     public static void main(String[] args) {
-        Keyboard obj1 = new Keyboard();
-        Keyboard obj2 = new Keyboard();
-        Keyboard obj3 = new Keyboard();
-        System.out.println("The number of objects are = " + Keyboard.count);
+        String str1 = new String("Hello");
+        str1.concat(" World");
+        System.out.println(str1); // Hello was printed although we concatenated " World" that means it is
+                                  // immutable
+
+        StringBuffer str2 = new StringBuffer("Hello");
+        str2.append(" World");
+        System.out.println(str2); // Hello World is printed
     }
 }
-
-// instance are the normal variables and these can be multiple for multiple copy
-// for objects
-// class variable will be one and it will be static
-// we cannot declare static as local
