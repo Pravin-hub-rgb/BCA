eaf466519cb448a5b4a20a85462856fa58838a5c
Pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2023-09-01 16:39:27 +0530

    notes update

--- resources/sem3/javatbc303/program/Test.java
+++ resources/sem3/javatbc303/program/Test.java
@@ -1,12 +1,11 @@
+import static java.lang.System.*;
+
+// Incorrect usage:
+// import static java.util.*; // This is not allowed
+
 class Test {
     public static void main(String[] args) {
-        String str1 = new String("Hello");
-        str1.concat(" World");
-        System.out.println(str1); // Hello was printed although we concatenated " World" that means it is
-                                  // immutable
-
-        StringBuffer str2 = new StringBuffer("Hello");
-        str2.append(" World");
-        System.out.println(str2); // Hello World is printed
+        out.println("Hello World"); // No need to use the keyword 'System' every time
+        out.println("How are you???");
     }
 }
