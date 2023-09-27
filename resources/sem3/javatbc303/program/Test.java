8879c8da8ffffc21fd7444329f4273b3d407669a
Pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2023-09-27 20:22:49 +0530

    notes update

--- resources/sem3/javatbc303/program/Test.java
+++ resources/sem3/javatbc303/program/Test.java
@@ -1,93 +1,25 @@
-// class Test {
-//     public static void main(String[] args) {
-//         StringBuilder str = new StringBuilder("Henry Ford!");
-//         strRev(str);
-//     }
-
-//     public static void strRev(StringBuilder str) {
-//         StringBuilder rev = new StringBuilder();
-//         for (int i = str.length() - 1; i >= 0; i--) {
-//             rev.append(str.charAt(i));
-//         }
-//         System.out.println(rev);
-//     }
-// }
-import java.util.Random;
-import java.util.Scanner;
-
 public class Test {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(() -> {
+            System.out.println("Thread 1: Running");
+        });
 
-    public static void main(String[] args) {
+        Thread thread2 = new Thread(() -> {
+            System.out.println("Thread 2: Running");
+        });
 
-        // A simple text scanner for user input...
-        @SuppressWarnings("resource")
-        Scanner scanner = new Scanner(System.in);
+        thread1.start(); // Thread 1 transitions from "New" to "Runnable" to "Running."
+        thread2.start(); // Thread 2 transitions from "New" to "Runnable" to "Running."
 
-        // Constructor to generate random number for the player...
-        Random random = new Random();
+        Thread.sleep(1000);
 
-        // Given Range of the random number to be generated & maximum attempt the play
-        // can have...
-        int minimum = 1;
-        int maximum = 100;
-        int maxAttempts = 10;
+        thread1.interrupt(); // Thread 1 transitions to "Blocked" if asleep, or "Runnable" if not interrupted.
+        Thread.sleep(1000);
 
-        // Variable to keep count for the final result...
-        int totalAttempts = 0;
-        int totalRounds = 0;
+        thread1.join(); // Wait for Thread 1 to finish its execution.
+        thread2.join(); // Wait for Thread 2 to finish its execution.
 
-        boolean playAgain = true;
-
-        System.out.println("WELCOME TO NUMBER GUESSING GAME");
-
-        while (playAgain) {
-            // Creating a random number by the computer for a given range between minimum &
-            // maximum...
-            int randomNumber = random.nextInt(maximum - minimum + 1) + minimum;
-            System.out.println("\nRound : " + (totalRounds + 1));
-            System.out.println("Computer has generate a random number between " + minimum + " and " + maximum);
-            System.out.println("Try to guess the number in " + maxAttempts + " attempts");
-
-            int attempt = 0;
-            boolean roundWon = false;
-
-            // do...while loop to check user number & computer generated number matches &
-            // count number of attempts...
-            do {
-                System.out.print("Attempt " + (attempt + 1) + "/" + maxAttempts + " : Guess the number : ");
-                int userGuess = scanner.nextInt();
-                attempt++;
-
-                if (userGuess > randomNumber) {
-                    System.out.println("The number is to high");
-                } else if (userGuess < randomNumber) {
-                    System.out.println("The number is to low");
-                } else {
-                    System.out.println("Congrulations!!! You guess the right number.");
-                    roundWon = true;
-                }
-            } while (!roundWon && attempt < maxAttempts);
-
-            // Keeping count of Total_Rounds & Total_attempt played by the user...
-            if (roundWon) {
-                totalRounds++;
-                totalAttempts += attempt;
-            } else {
-                System.out.println("Sorry! You were unable to guess the number. The number was : " + randomNumber);
-            }
-
-            // Asking user to play the game again...
-            System.out.println("Do you want to play again [yes/no]");
-            String playAgainChoice = scanner.next();
-            playAgain = playAgainChoice.equalsIgnoreCase("yes");
-        }
-
-        // Displaying the final score of the game...
-        System.out.println("Game Over ! Thank you for playing.");
-        System.out.println("The Final Score is ");
-        System.out.println("Total Rounds Won : " + totalRounds);
-        System.out.println("Total Attempts : " + totalAttempts);
-
+        System.out.println("Thread 1 state: " + thread1.getState());
+        System.out.println("Thread 2 state: " + thread2.getState());
     }
-
 }
