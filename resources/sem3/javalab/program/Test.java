ed5261e5714d0a2702d3fc243cd037b69b2c3e3d
Pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2023-10-02 12:58:32 +0530

    notes update

--- resources/sem3/javalab/program/Test.java
+++ resources/sem3/javalab/program/Test.java
@@ -1,32 +1,18 @@
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.*;
 
-class Test
-{
+class Test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num1 = 0;
         int num2 = 0;
         int sum = 0;
-        try
-        {
-            System.out.println("Enter first number : ");
+        
+        System.out.println("Enter first number : ");
         num1 = sc.nextInt();
         System.out.println("Enter second number : ");
         num2 = sc.nextInt();
-        }
-        catch(InputMismatchException e)
-        {
-            System.out.println(e);
-        }
-        try
-        {
-            String str = "123Human";
-            int num3 = Integer.parseInt(str);
-        } 
-        catch(NumberFormatException e)
-        {
-            System.out.println(e);
-        }
+
+        String str = "123Human";
+        int num3 = Integer.parseInt(str);
     }
 }
