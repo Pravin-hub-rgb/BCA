b33107d697e8874f8d906f659c5cbbd08309574b
Pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2023-10-02 20:51:49 +0530

    notes update

--- resources/sem3/javalab/program/Test.java
+++ resources/sem3/javalab/program/Test.java
@@ -1,21 +1,26 @@
-
 class MyException extends Exception {
-    MyException(String str) {
-        super(str);
+    public MyException(String message) {
+        super(message);
     }
 }
 
 class Test {
+    // This method declares that it may throw a custom exception.
+    public void divide(int numerator, int denominator) throws MyException {
+        if (denominator == 0) {
+            // Simulate a custom exception (for demonstration purposes).
+            throw new MyException("Division by zero is not allowed.");
+        }
+        int result = numerator / denominator;
+        System.out.println("Result: " + result);
+    }
+
     public static void main(String[] args) {
-        int n1 = 10;
+        Test myObj = new Test();
         try {
-            if (n1 % 2 == 0) {
-                throw new MyException("Number is even.");
-            } else {
-                System.out.println("Nothing is wrong");
-            }
+            myObj.divide(10, 0);
         } catch (MyException e) {
-            System.out.println(e);
+            System.out.println("Caught an exception: " + e.getMessage());
         }
     }
 }
