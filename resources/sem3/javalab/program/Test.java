8d53f40ee78aaf7095c8e2c77734c950cdd4e39a
Pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2023-10-02 18:00:49 +0530

    notes update

--- resources/sem3/javalab/program/Test.java
+++ resources/sem3/javalab/program/Test.java
@@ -1,18 +1,21 @@
-import java.util.*;
+
+class MyException extends Exception {
+    MyException(String str) {
+        super(str);
+    }
+}
 
 class Test {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num1 = 0;
-        int num2 = 0;
-        int sum = 0;
-        
-        System.out.println("Enter first number : ");
-        num1 = sc.nextInt();
-        System.out.println("Enter second number : ");
-        num2 = sc.nextInt();
-
-        String str = "123Human";
-        int num3 = Integer.parseInt(str);
+        int n1 = 10;
+        try {
+            if (n1 % 2 == 0) {
+                throw new MyException("Number is even.");
+            } else {
+                System.out.println("Nothing is wrong");
+            }
+        } catch (MyException e) {
+            System.out.println(e);
+        }
     }
 }
