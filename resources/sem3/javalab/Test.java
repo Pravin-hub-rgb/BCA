2c7194ac0e0e60c2ce46aab772a2567068f44467
Pravinx0 <pravvinkumarkumar@gmail.com>
2023-12-12 20:18:19 +0530

    notes update

--- resources/sem3/javalab/Test.java
+++ resources/sem3/javalab/Test.java
@@ -1,65 +1,21 @@
-import java.util.Scanner;
-
-interface Account {
-    public void deposit();
-
-    public void withdraw();
-
-    public void displayBalances();
-}
-
-class SavingsAccount implements Account {
-    Scanner sc = new Scanner(System.in);
-    int wallet = 0;
-
-    public void deposit() {
-        System.out.println("Enter amount to deposit : ");
-        wallet += sc.nextInt();
+class Test {
+    public static void main(String[] args) {
+        int x = 10, y = 20;
+        double l = 100, m = 200;
+        System.out.println(addition(x, y));
+        System.out.println(addition(l, m));
+        System.out.println(addition(x, m));
     }
 
-    public void withdraw() {
-        System.out.println("Enter the amount you want to withdraw : ");
-        int amount = sc.nextInt();
-        wallet -= amount;
+    static int addition(int a, int b) {
+        return a + b;
     }
 
-    public void displayBalances() {
-        System.out.println("Your Balance is = " + wallet);
-    }
-}
-
-class CurrentAccount implements Account {
-    Scanner sc = new Scanner(System.in);
-    int wallet = 0;
-
-    public void deposit() {
-        System.out.println("Enter amount to deposit : ");
-        wallet += sc.nextInt();
+    static int addition(double a, double b) {
+        return (int) a + (int) b;
     }
 
-    public void withdraw() {
-        System.out.println("Enter the amount you want to withdraw : ");
-        int amount = sc.nextInt();
-        wallet -= amount;
-    }
-
-    public void displayBalances() {
-        System.out.println("Your Balance is = " + wallet);
-    }
-}
-
-class Test
-{
-    public static void main(String[] args)
-    {
-        SavingsAccount sa = new SavingsAccount();
-        sa.deposit();
-        sa.withdraw();
-        sa.displayBalances();
-
-        CurrentAccount ca = new CurrentAccount();
-        ca.deposit();
-        ca.withdraw();
-        ca.displayBalances();
+    static int addition(int a, double b) {
+        return a + (int) b;
     }
 }
