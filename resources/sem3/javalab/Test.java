b49312b9483a8161d984d99799d323ef570ab046
Pravinx0 <pravvinkumarkumar@gmail.com>
2023-12-12 09:44:16 +0530

    notes update:

--- resources/sem3/javalab/Test.java
+++ resources/sem3/javalab/Test.java
@@ -4,15 +4,15 @@ public class Test
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
+        int num, sum = 0;
         System.out.println("Enter a number : ");
-        int a = sc.nextInt();
-
-        int temp = a, count = 0;
+        num = sc.nextInt();
+        int temp = num;
         while(temp > 0)
         {
-            count++;
+            sum += temp%10;
             temp /= 10;
         }
-        System.out.println("The number of digits = " + count);
+        System.out.println("The sum is = " + sum);
     }
 }
