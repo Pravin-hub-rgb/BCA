88bbba8f752b842b3eb6f3782763e40a599079a2
Pravinx0 <pravvinkumarkumar@gmail.com>
2023-12-12 23:55:37 +0530

    notes update

--- resources/sem3/javalab/Test.java
+++ resources/sem3/javalab/Test.java
@@ -1,16 +1,24 @@
 import java.awt.*;
-
 class Test
 {
     public static void main(String[] args)
     {
         Frame frame = new Frame("New Window");
-        Label label = new Label("Number 1 : ");
-        Button btn1 = new Button("Add");
+        FlowLayout fl = new FlowLayout();
+        Label lab1 = new Label("Name : ");
+        Label lab2 = new Label("Password : ");
+        TextField tf1 = new TextField(20);
+        TextField tf2 = new TextField(20);
+        Button btn1 = new Button("Submit");
 
-        frame.add(label);
+        frame.add(lab1);
+        frame.add(tf1);
+        frame.add(lab2);
+        frame.add(tf2);
         frame.add(btn1);
+
+        frame.setLayout(fl);
         frame.setVisible(true);
-        frame.setSize(400, 200);
+        frame.setSize(300, 200);
     }
 }
