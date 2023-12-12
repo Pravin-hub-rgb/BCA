46ede7e8130b6113de6105790e03153adeb634de
Pravinx0 <pravvinkumarkumar@gmail.com>
2023-12-12 11:47:22 +0530

    notes update:

--- resources/sem3/javalab/Test.java
+++ resources/sem3/javalab/Test.java
@@ -2,26 +2,9 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a;
-        System.out.println("Enter a number : ");
-        a = sc.nextInt();
-        if (checkPalindrome(a)) {
-            System.out.println("It is a palindrome");
-        } else {
-            System.out.println("Not a palindrome");
-        }
-    }
-
-    static boolean checkPalindrome(int a) {
-        int rev = 0, temp = a;
-        while (temp > 0) {
-            rev *= 10;
-            rev += temp % 10;
-            temp /= 10;
-        }
-        if (rev == a)
-            return true;
-        return false;
+        int arrayname[];
+        int[] newarray;
+        newarray = new int[7];
+        int[] myarray = new int[];
     }
 }
