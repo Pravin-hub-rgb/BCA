2f6769d3418bef2cf7671d75d6530f6caf236e7d
Pravinx0 <pravvinkumarkumar@gmail.com>
2023-12-12 12:03:19 +0530

    notes update

--- resources/sem3/javalab/Test.java
+++ resources/sem3/javalab/Test.java
@@ -2,10 +2,24 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-        int arrayname[];
-        int[] newarray;
-        newarray = new int[7];
-        int[] myarray = new int[9];
-        int[] newarray1 = { 1, 2, 3, 4, 5 };
+        int[][] arr1 = {{1, 2}, {3, 4}};
+        int[][] arr2 = {{2, 3}, {3, 4}};
+        int[][] sum = new int[2][2];
+        for(int i = 0; i < 2; i++)
+        {
+            for(int j = 0; j < 2; j++)
+            {
+                sum[i][j] += arr1[i][j] + arr2[i][j];
+            }
+        }
+        System.out.println("Displaying the sum of array : ");
+        for(int i = 0; i < 2; i++)
+        {
+            for(int j = 0; j < 2; j++)
+            {
+                System.out.print(sum[i][j] + " ");
+            }
+            System.out.println("");
+        }
     }
 }
