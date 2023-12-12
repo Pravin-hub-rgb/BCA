e35dea4e8f255954ab544c2c2b15ebac0749a338
Pravin <pravinx0@pop-os.localdomain>
2023-12-12 17:32:00 +0530

    notes update

--- resources/sem3/javalab/Test.java
+++ resources/sem3/javalab/Test.java
@@ -3,24 +3,12 @@ import java.util.Scanner;
 public class Test {
     public static void main(String[] args)
     {
-        int[] arr1 = {1, 2, 3, 4};
-        int[] arr2 = {2, 3, 4, 5};
-        sumArray(arr1, arr2);
-    }
-    static void sumArray(int[] a1, int[] a2)
-    {
-        int[] sum = new int[a1.length];
-        for(int i = 0; i < a1.length; i++)
-        {
-            sum[i] = a1[i] + a2[i];
-        }
-        displayArr(sum);
-    }
-    static void displayArr(int[] s)
-    {
-        for(int i = 0; i < s.length; i++)
-        {
-            System.out.print(s[i] + " ");
-        }
+        String str1 = new String("Hello");
+        str1 = str1.concat(" world");
+        System.out.println(str1);
+
+        StringBuffer str2 = new StringBuffer("Hello");
+        str2.append(" world");
+        System.out.println(str2);
     }
 }
