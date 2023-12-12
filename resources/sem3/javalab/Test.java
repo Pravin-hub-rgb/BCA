0d1c69e4ce044385ee1f61dab87fc7ca6e3c6e5a
Pravinx0 <pravvinkumarkumar@gmail.com>
2023-12-12 21:21:36 +0530

    notes update

--- resources/sem3/javalab/Test.java
+++ resources/sem3/javalab/Test.java
@@ -1,20 +1,18 @@
-import group.Shape.*;
-class Test{
-    public static void main(String[] args)
-    {
-        group.Shape obj = new group.Shape();
-        obj.sqarea();
-    }
-    static void area(int side)
-    {
-        System.out.println("The area of square = " + side * side);
-    }
-    static void area(int len, int bred)
-    {
-        System.out.println("The area of rectangle : " + len * bred);
-    }
-    static void area(double radius)
-    {
-        System.out.println("The area of circle = " + 3.14 * radius * radius);
+import java.util.Scanner;
+
+class Test {
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(System.in);
+            int v1, v2, ans = 0;
+            System.out.println("Enter first number : ");
+            v1 = sc.nextInt();
+            System.out.println("second number : ");
+            v2 = sc.nextInt();
+            ans = v1 / v2;
+            System.out.println(ans);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
