b8818c71345ea77f5ec4c357bb3bdc7d814979fb
Pravinx0 <pravvinkumarkumar@gmail.com>
2023-12-12 11:43:31 +0530

    notes update:

--- resources/sem3/javalab/Test.java
+++ resources/sem3/javalab/Test.java
@@ -1,26 +1,27 @@
 import java.util.Scanner;
-public class Test
-{
-    public static void main(String[] args)
-    {
+
+public class Test {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a;
         System.out.println("Enter a number : ");
         a = sc.nextInt();
+        if (checkPalindrome(a)) {
+            System.out.println("It is a palindrome");
+        } else {
+            System.out.println("Not a palindrome");
+        }
+    }
+
+    static boolean checkPalindrome(int a) {
         int rev = 0, temp = a;
-        while(temp > 0)
-        {
+        while (temp > 0) {
             rev *= 10;
             rev += temp % 10;
             temp /= 10;
         }
-        System.out.println("The reverse is = " + rev);
-            if(rev == a)
-            {
-                System.out.println("The given number is a palindrome");
-            } else 
-            {
-                System.out.println("The given number is not a palindrome");
-            }
+        if (rev == a)
+            return true;
+        return false;
     }
 }
