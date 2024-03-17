16eee74700ba2b6b7d10caf40d8c511c7cd7d850
Pravinx0 <pravvinkumarkumar@gmail.com>
2024-03-17 10:32:26 +0530

    notes update

--- resources/sem4/pythonlab/program/test.py
+++ resources/sem4/pythonlab/program/test.py
@@ -1,5 +1,9 @@
-a = 9//2
-b = 9/2
-print("9//2 = ",a," 9/2 = ", b)
-c = 9**2
-print("9**2 = ", c)
+mat1 = [[2, 3, 66], [4, 5, 55]]
+mat2 = [[2, 2, 54], [3, 3, 33]]
+sum = [[0, 0, 0], [0, 0, 0]]
+
+for i in range(len(mat1)):
+    for j in range(len(mat1[0])):
+        sum[i][j] = mat1[i][j] + mat2[i][j]
+
+print(sum)
