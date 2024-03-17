a1fb8c29d1a05ca011b35690d4b97aba19253ec8
Pravinx0 <pravvinkumarkumar@gmail.com>
2024-03-17 10:16:11 +0530

    notes update

--- resources/sem4/pythonlab/program/test.py
+++ resources/sem4/pythonlab/program/test.py
@@ -1,7 +1,5 @@
-ch = input("Enter a character:")
-if 'a' <= ch <= 'z':
-    print("Entered charcter is lower case letter")
-elif 'A' <= ch <= 'Z':
-    print("Enter character is upper case letter")
-else:
-    print("Entered character is not an Alphabet")
+a = 9//2
+b = 9/2
+print("9//2 = ",a," 9/2 = ", b)
+c = 9**2
+print("9**2 = ", c)
