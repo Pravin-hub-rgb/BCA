0fd88453d130222572f2d3aef0fd7a2e0d15cfba
Pravinx0 <pravvinkumarkumar@gmail.com>
2024-03-17 12:35:05 +0530

    notes update

--- resources/sem4/pythonlab/program/test.py
+++ resources/sem4/pythonlab/program/test.py
@@ -1,9 +1,8 @@
-mat1 = [[2, 3, 66], [4, 5, 55]]
-mat2 = [[2, 2, 54], [3, 3, 33]]
-sum = [[0, 0, 0], [0, 0, 0]]
-
-for i in range(len(mat1)):
-    for j in range(len(mat1[0])):
-        sum[i][j] = mat1[i][j] + mat2[i][j]
-
-print(sum)
+def checkNum(a):
+    if a%2==0:
+        print("Number is even")
+    else:
+        print("Number is odd")
+        
+num = int(input("Enter a number : "))
+checkNum(num)
