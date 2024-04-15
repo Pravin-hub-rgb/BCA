b329f13a8656d7c0190e8af21e275fc608e31b90
Pravinx0 <pravvinkumarkumar@gmail.com>
2024-04-15 11:52:33 +0530

    notes udpate

--- resources/sem4/pythonlab/program/test.py
+++ resources/sem4/pythonlab/program/test.py
@@ -1,19 +1,4 @@
-mylist = [556, "Mothi", 84, 96, 84, 75, 84]
-
-#Access 0th element
-print(mylist[0])
-
-#Access 0th to first element
-print(mylist[0:2])
-
-#Access 2nd to end of the list
-print(mylist[1:])
-
-#Accessing starting to ending element
-print(mylist[:])
-
-#Access last index value
-print(mylist[-1])
-
-#Access element in reverse order
-print(mylist[::-1])
+for i in range(5):
+    for j in range(i+1):
+        print(j+1, end='')
+    print()
