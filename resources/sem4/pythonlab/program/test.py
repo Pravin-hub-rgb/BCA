848c99d64d6d90c4bccd4ecc1eb73a2379900d63
Pravinx0 <pravvinkumarkumar@gmail.com>
2024-04-16 09:45:50 +0530

    notes update

--- resources/sem4/pythonlab/program/test.py
+++ resources/sem4/pythonlab/program/test.py
@@ -1,4 +1,14 @@
-for i in range(5):
-    for j in range(i+1):
-        print(j+1, end='')
+n = 5
+for i in range(n):
+    for j in range(n-i-1):
+        print(" ", end='')
+    for k in range(i+1):
+        print("*", end='')
     print()
+# n = 5
+# for i in range(n):
+#     for j in range(n - i - 1):
+#         print(" ", end='')
+#     for k in range(i + 1):
+#         print("*", end='')
+#     print()
