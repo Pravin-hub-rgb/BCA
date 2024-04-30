7b0564bb4d054aa24879f95bfea87b65b4128ba6
Pravinx0 <pravvinkumarkumar@gmail.com>
2024-04-30 21:22:06 +0530

    notes update

--- resources/sem4/pythonlab/program/test7.py
+++
@@ -1,9 +0,0 @@
-def swapFirstLast(str):
-    first_char = str[0]
-    last_char = str[-1]
-    newStr = last_char + str[1:-1] + first_char
-    return newStr
-
-myStr = "menance"
-
-print(swapFirstLast(myStr))
