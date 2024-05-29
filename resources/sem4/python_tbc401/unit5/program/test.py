a5ae0d0e68e88a9f448a59c750d93402e80a5a44
Pravinx0 <pravvinkumarkumar@gmail.com>
2024-05-29 19:51:57 +0530

    notes update

--- resources/sem4/python_tbc401/unit5/program/test.py
+++ resources/sem4/python_tbc401/unit5/program/test.py
@@ -1,3 +1,4 @@
 import pandas as pd
-df = pd.read_csv('Salary_Data.csv')
-print(df.tail())
+df = pd.read_csv('sample.csv')
+df2 = df.fillna({'Physics': 'none', 'Chemistry': 0, 'Maths': 30})
+print(df2)
