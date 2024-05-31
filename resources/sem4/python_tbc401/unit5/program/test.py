32ca81e9948cbc119dcf90de003d8e8f5dab8c13
pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2024-05-31 16:21:37 +0530

    notes update

--- resources/sem4/python_tbc401/unit5/program/test.py
+++ resources/sem4/python_tbc401/unit5/program/test.py
@@ -1,3 +1,9 @@
-import pandas as pd
-df = pd.read_csv('sample2.csv', index_col=['Roll No.'])
-print(df.loc[[2,4,6]])
+import matplotlib.pyplot as plt
+
+data = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+plt.boxplot(data)
+plt.xlabel('Data')
+plt.ylabel('Values')
+plt.title('Box Plot Example')
+plt.show()
