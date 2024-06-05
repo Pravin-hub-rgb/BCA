20056bb77329f4fd9a1e2876c8dea5b228d1c023
pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2024-06-05 11:16:00 +0530

    notes update

--- resources/sem4/python_tbc401/unit5/program/test.py
+++ resources/sem4/python_tbc401/unit5/program/test.py
@@ -1,8 +1,8 @@
+
 import pandas as pd
 
-# Creating a sample DataFrame
-data = {'Name': ['John', 'Emma', 'Michael', 'Sophia'],
-         'Age': [28, 24, 32, 29],
-        'City': ['New York', 'San Francisco', 'Los Angeles', 'Houston']}
-df = pd.DataFrame(data)
-df.to_csv('Dataframe1.csv', index=False)
+# Loading and reading the original DataFrame
+sharad = pd.read_csv('dirtydata.csv')
+
+# Discovering duplicate values
+print(sharad.duplicated())
