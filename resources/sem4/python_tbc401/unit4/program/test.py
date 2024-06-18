986badd52188efb9d81168e27be7a2bd0657d2c8
pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2024-06-18 21:54:26 +0530

    notes update Python

--- resources/sem4/python_tbc401/unit4/program/test.py
+++ resources/sem4/python_tbc401/unit4/program/test.py
@@ -1,3 +1,15 @@
-import pandas as pd
-mySeries = pd.Series({'a':1,'b':2,'c':3,'d':4,'e':5})
-print(max(mySeries))
+import math
+n = int(input("Enter n : "))
+flag = True
+if n < 2:
+    print("Not prime")
+    exit(0)
+else:
+    for i in range(2,int(math.sqrt(n)) + 1):
+        if(n%i==0):
+                flag = False 
+                break
+if(flag):
+    print("Is prime")
+else:
+    print("Not prime")
