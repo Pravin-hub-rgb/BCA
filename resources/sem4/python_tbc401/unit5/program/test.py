015fea0d2f96cb454efb7246ec05de09ad77a353
pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2024-06-18 12:07:02 +0530

    matplotlib examples add python

--- resources/sem4/python_tbc401/unit5/program/test.py
+++ resources/sem4/python_tbc401/unit5/program/test.py
@@ -1,8 +1,14 @@
 
-import pandas as pd
+import matplotlib.pyplot as plt
 
-# Loading and reading the original DataFrame
-sharad = pd.read_csv('dirtydata.csv')
+# Predefined data sets
+data1 = [10, 20, 30, 40, 50]
+data2 = [15, 25, 35, 45, 55]
+data3 = [5, 15, 25, 35, 45]
 
-# Discovering duplicate values
-print(sharad.duplicated())
+plt.boxplot([data1, data2, data3], labels=['Data 1', 'Data 2', 'Data 3'])
+plt.xlabel('Data Sets')
+plt.ylabel('Values')
+plt.title('Multiple Box Plots Example')
+plt.show()
+              
