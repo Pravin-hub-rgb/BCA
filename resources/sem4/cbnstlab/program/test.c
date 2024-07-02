a91a1e7801829a83958b540944c62066f1a76cdc
pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2024-07-02 18:23:26 +0530

    notes update

--- resources/sem4/cbnstlab/program/test.c
+++ resources/sem4/cbnstlab/program/test.c
@@ -1,58 +1,29 @@
-/* #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#define EPSILON 0.003
-float f(float x)
-{
-    return x * x * x - 4 * x - 9;
-}
-float df(float x)
-{
-    return 3 * x * x - 4;
-}
-
-int main()
-{
-    float x0, x1;
-    int i = 1;
-    printf("Enter Initial Guess: ");
-    scanf("%f", &x0);
-    do
-    {
-        x1 = x0 - (f(x0)/df(x0));
-        x0 = x1;
-        printf("Root after iteration = %d is %f and value of function = %f\n", i, x1, f(x1));
-        i++;
-    } while (fabs(f(x1)) > EPSILON);
-    return 0;
-}
-*/
 
 #include <stdio.h>
-#include <math.h>
-#define EPSILON 0.003
 
-float f(float x)
+float findValueAt(float x)
 {
-    return ((x * x * x) - 4 * x - 9);
-}
-float df(float x)
-{
-    return (3 * x * x - 4);
+    return x * x * x;
 }
 
 int main()
 {
-    float x0, x1;
-    int i;
-    printf("Enter intial guess: ");
-    scanf("%f", &x0);
-    do
+    int n;
+    float sum = 0, h, a, b, i;
+    printf("Enter the upper limit value : ");
+    scanf("%f", &a);
+    printf("Enter the lower limit value : ");
+    scanf("%f", &b);
+    printf("Enter the number of Intervals : ");
+    scanf("%d", &n);
+    sum = findValueAt(a) + findValueAt(b);
+    h = (a + b) / n;
+    for (i = a + h; i < b; i = i + h)
     {
-        x1 = x0 - (f(x0) / df(x0));
-        x0 = x1;
-        i++;
-        printf("No. of iteration = %d\t Root = %f\t Value of function = %f\n", i, x1, f(x1));
-    } while (fabs(f(x1)) > EPSILON);
+        sum = sum + (2 * findValueAt(i));
+    }
+    sum = (sum * h) / 2;
+    printf("Value of the Integral is = %f\n", sum);
     return 0;
 }
+                        
