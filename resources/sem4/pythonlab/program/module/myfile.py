780663a630c45b5134bd0f09e37a09498162affd
pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2024-07-04 06:34:37 +0530

    notes update

--- resources/sem4/pythonlab/program/module/myfile.py
+++ resources/sem4/pythonlab/program/module/myfile.py
@@ -1,7 +1,5 @@
-import mymodule
 import numpy as np
-arr1 = np.array([[1,2,3],[3,3,3],[0,0,0]])
-arr2 = np.array([[1,2,3],[3,3,3],[0,0,0]])
-
-sum = mymodule.sum_array(arr1, arr2)
-print(sum)
+mylist=[1,2,3,4]
+myarr = np.array([1,3,4,5])
+print(type(mylist))
+print(type(myarr))
--- resources/sem4/pythonlab/program/module/mymodule.py
+++ resources/sem4/pythonlab/program/module/mymodule.py
@@ -1,8 +1,8 @@
 # calculator.py
 import numpy as np
 def sum_array(a1, a2):
-    m=np.arange(9)
-    m=m.resize(3,3)
+    m=np.arange(1,10)
+    m=m.reshape(3,3)
     for i in range(len(a1)):
         for j in range(len(a1[0])):
             m[i][j] = a1[i][j] + a2[i][j]
