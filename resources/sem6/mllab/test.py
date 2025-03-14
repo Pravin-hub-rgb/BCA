fceb5250d47a7385798687fbed849e471e711bc2
pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2025-03-14 19:16:04 +0530

    notes update

--- resources/sem6/mllab/test.py
+++ resources/sem6/mllab/test.py
@@ -26,13 +26,13 @@ else:
     # Write a function to check whether a given number is a palindrome or not. X
     # WAP function to sum all the numbers in a given list. X
     # Write a function to print the minimum and maximum numbers from a given array. X
-    # Write a function to perform a linear search.
-    # WAP to print prime numbers from 1 to 300.
+    # Write a function to perform a linear search. X
+    # WAP to print prime numbers from 1 to 300. X
     # WAP to print the user information.
     # ********************************
-    # Check if a string is a palindrome
-    # Generate the Fibonacci sequence up to n terms.
-    # Count the frequency of each word in a given text.
+    # Check if a string is a palindrome X
+    # Generate the Fibonacci sequence up to n terms. X
+    # Count the frequency of each word in a given text. X
     # Check the strength of a password based on rules (e.g., length, uppercase, lowercase, digits, special characters).
     # Count the number of words in a text file.
     # Check if a number is prime.
