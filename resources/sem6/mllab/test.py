350e727c9cd32e856aeab7cd53ffd9e62bf8c7ee
pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2025-03-14 15:33:56 +0530

    notes update

--- resources/sem6/mllab/test.py
+++ resources/sem6/mllab/test.py
@@ -1,7 +1,47 @@
-def fact(n):
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return n * fact(n - 1)
+import numpy as np
 
-print(f"Factorial of 5 = {fact(5)}")
+my_arr = np.array([2,3,4,5,1,55])
+
+def min_max_arr(arr):
+    max = arr[0]
+    min = arr[0]
+    for ele in arr:
+        if ele < min:
+            min = ele 
+        elif ele > max:
+            max = ele 
+    print(f"Max = {max}")
+    print(f"Min = {min}")
+
+min_max_arr(my_arr)
+
+
+    # WAP to add two numbers. X
+    # WAP to check whether a number is even or odd. X
+    # WAP to find the largest number among three numbers. X
+    # WAP to swap two numbers without using a third variable. X
+    # Write a function to print the table of a given number. X
+    # WAP to generate a random number between 0 and 100. X
+    # WAP to print the factorial of a given number. X
+    # Write a function to check whether a given number is a palindrome or not. X
+    # WAP function to sum all the numbers in a given list. X
+    # Write a function to print the minimum and maximum numbers from a given array.
+    # Write a function to perform a linear search.
+    # WAP to print prime numbers from 1 to 300.
+    # WAP to print the user information.
+    # ********************************
+    # Check if a string is a palindrome
+    # Generate the Fibonacci sequence up to n terms.
+    # Count the frequency of each word in a given text.
+    # Check the strength of a password based on rules (e.g., length, uppercase, lowercase, digits, special characters).
+    # Count the number of words in a text file.
+    # Check if a number is prime.
+    # Reverse a string without using built-in functions.
+    # Find the second largest number in a list.
+    # Merge two dictionaries into one.
+    # Find common elements between two lists.
+    # Remove duplicates from a list while preserving the order.
+    # Check if two strings are anagrams (contain the same characters in a different order
+    # Generate a random password of a specified length.
+    # Find the longest word in a given sentence.
+    # Calculate the factorial of a number using recursion.
