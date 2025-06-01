b63a4c1283309b0c79207c830653cd894dd36da1
pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2025-06-01 11:36:51 +0530

    Save local changes before pull

--- resources/sem6/mllab/test.py
+++ resources/sem6/mllab/test.py
@@ -1,3 +1,12 @@
-num_float = 5.7
-num_int = int(num_float)
-print(num_int)
+import numpy as np 
+
+n = int(input("Enter the number of element"))
+
+elements = []
+
+for i in range(n):
+    element = int(input(f"Enter element {i + 1}: "))
+    elements.append(element)
+
+elements_array = np.array(elements)
+print(elements_array)
